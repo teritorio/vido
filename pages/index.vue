168fb5361e0302023a9388a9426ada7f4a565078
Guillaume AMAT <guillaume@amat.io>
2021-01-23 01:14:19 +0100

    feat: add the logo and adjust some spaces

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="flex">
     <div class="w-1/2 px-16 py-8">
-      <h1 class="uppercase font-bold">Titre</h1>
+      <h1>
+        <Logo
+          class="w-40 h-auto fill-current text-gray-900"
+          aria-label="Landes, Terre des possibles"
+        />
+      </h1>
 
-      <div class="w-full max-w-2xl mx-auto my-4">
+      <div class="w-full max-w-2xl mx-auto my-10">
         <form class="flex space-x-3">
           <input
             class="flex-1 appearance-none border border-transparent w-full py-3 px-7 bg-gray-100 text-gray-700 placeholder-gray-800 text-lg rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             placeholder="Recherche..."
           />
         </form>
-        <div class="flex justify-center mt-4 space-x-6 font-medium">
+        <div class="flex justify-center mt-4 space-x-6">
           <p>Il est presque midi, vous n'avez pas faim ?</p>
           <a href="#" class="text-blue-500 hover:underline"
             >Voir les restaurants</a
@@ -135,15 +140,17 @@
 <script lang="ts">
 import Vue from 'vue'
 
+import LeisureParkGardenIcon from '@/assets/leisure-park-garden•.svg?inline'
+import Logo from '@/assets/logo-white.svg?inline'
 import Map from '@/components/Map.vue'
 import OpenMention from '@/components/OpenMention.vue'
 import ClosedMention from '@/components/ClosedMention.vue'
-import LeisureParkGardenIcon from '@/assets/leisure-park-garden•.svg?inline'
 
 export default Vue.extend({
   components: {
     ClosedMention,
     LeisureParkGardenIcon,
+    Logo,
     Map,
     OpenMention,
   },
