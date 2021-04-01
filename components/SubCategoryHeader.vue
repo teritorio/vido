86bed553f81ace81614431cdcc1cbd566421c012
Guillaume AMAT <g.amat@teritorio.fr>
2021-04-01 23:53:57 +0200

    feat: remove the margin-top on sub-categories buttons

--- components/SubCategoryHeader.vue
+++ components/SubCategoryHeader.vue
@@ -2,9 +2,7 @@
   <header
     class="fixed top-0 bottom-0 flex flex-col w-full h-full max-w-md p-4 space-y-4 pointer-events-none"
   >
-    <aside
-      class="px-5 py-4 space-y-12 bg-white shadow-md pointer-events-auto rounded-xl"
-    >
+    <aside class="px-5 py-4 bg-white shadow-md pointer-events-auto rounded-xl">
       <button
         type="button"
         class="flex items-center justify-center w-10 h-10 text-2xl font-bold transition-all rounded-full cursor-pointer focus:outline-none hover:bg-gray-100 focus:bg-gray-100"
