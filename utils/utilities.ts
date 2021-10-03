a44b49fd5b28484ac200775dfaccce83ac360356
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-10-03 13:46:02 +0200

    fix: typ ts in flattenFeatures()

--- utils/utilities.ts
+++ utils/utilities.ts
@@ -1,6 +1,8 @@
 import { VidoFeature } from '@/utils/types'
 
-export const flattenFeatures = (features: [VidoFeature]) =>
+export const flattenFeatures = (features: {
+  [categoryId: string]: VidoFeature[]
+}) =>
   Object.values(features)
     .flat()
     .filter((f: VidoFeature) => f.properties.vido_visible)
