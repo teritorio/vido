6850fe3fbebc0ebe47033da29bd57b2e844ea1ec
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-10-16 14:46:49 +0200

    fix: ignore ts typing error in index

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,11 @@ export default Vue.extend({
   components: {
     Home,
   },
-  data() {
+  data(): {
+    cssUrl: string
+    favicon: string
+    title: string
+  } {
     return {
       cssUrl: 'territorio',
       favicon: '',
@@ -32,22 +36,28 @@ export default Vue.extend({
     await fetch(`${this.$config.API_ENDPOINT}/geodata/v1/site`)
       .then((res) => res.json())
       .then((json) => {
+        // @ts-ignore - Look ok, unable to fix the issue
         this.cssUrl = json?.fr?.['teritorio-font']
+        // @ts-ignore - Look ok, unable to fix the issue
         this.favicon = json?.fr?.site1?.favicon
+        // @ts-ignore - Look ok, unable to fix the issue
         this.title = json?.fr?.site1?.title
       })
   },
   // fetchOnServer: false,
   head() {
     return {
+      // @ts-ignore - Look ok, unable to fix the issue
       title: this.title,
       link: [
         {
           rel: 'stylesheet',
+          // @ts-ignore - Look ok, unable to fix the issue
           href: this.cssUrl,
         },
         {
           rel: 'icon',
+          // @ts-ignore - Look ok, unable to fix the issue
           href: this.favicon,
         },
       ],
