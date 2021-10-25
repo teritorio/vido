2d718383271f0db808675f2fd1e75d823031ffba
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-10-25 22:36:43 +0200

    feat: generate js map

--- nuxt.config.js
+++ nuxt.config.js
@@ -117,6 +117,12 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    extend(config, { isClient }) {
+      // Extend only webpack config for client-bundle
+      if (isClient) {
+        config.devtool = 'source-map'
+      }
+    },
     plugins: [
       new webpack.ProvidePlugin({
         mapboxgl: 'maplibre-gl',
