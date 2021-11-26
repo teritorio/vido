1a66d6ac6d29cd57ee322a9dfb861382f6d6bfb6
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-11-26 19:07:30 +0100

    fix: config.NOTEBOOK_ENABLED teritorio/vido-private#450

--- components/FavoriteMenu.vue
+++ components/FavoriteMenu.vue
@@ -79,7 +79,7 @@
           {{ $tc('favorites.menu_share') }}
         </button>
         <button
-          v-if="$config.NOTEBOOK_ENABLED === 'true'"
+          v-if="$config.NOTEBOOK_ENABLED"
           class="block w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
           role="menuitem"
           @blur="blurHandler"
