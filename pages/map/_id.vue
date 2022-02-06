328f18b90acf3e1cc01e0968fdf0879c42c98540
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-02-06 22:39:58 +0100

    fix: remove default css

--- pages/map/_id.vue
+++ pages/map/_id.vue
@@ -87,6 +87,37 @@ export default Vue.extend({
 </script>
 
 <style>
+html {
+  @apply h-full w-full box-border overflow-hidden overscroll-none;
+}
+
+html,
+.mapboxgl-map {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  @apply text-gray-900;
+}
+
+body,
+#__nuxt,
+#__layout {
+  @apply h-full w-full overflow-hidden overscroll-none;
+}
+
+*,
+*::before,
+*::after {
+  @apply m-0 box-border;
+}
+</style>
+
+<style>
 #map {
   position: absolute;
   top: 0;
