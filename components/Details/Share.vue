fb558b3ce5d8b63b5d84e22fa3346c7e24db1d8b
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-02-13 20:00:07 +0100

    fix: share link & print

--- components/Details/Share.vue
+++ components/Details/Share.vue
@@ -1,73 +1,91 @@
 <template>
-  <ul>
-    <li>
-      <button
-        v-if="title && href"
-        aria-label="Partager sur Facebook"
-        type="button"
-        class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
-      >
-        <!-- href="https://www.facebook.com/share.php?u=https%3A%2F%2Fcarte.bidarttourisme.com%2Fcontent%2Fwp_poi%2Ftoki-toki%2F%3Fsite%3Dteritorio" -->
-        <font-awesome-icon
-          :icon="['fab', 'facebook']"
-          :style="{ color: color }"
-        />
-      </button>
-    </li>
-    <li>
-      <button
-        v-if="title && href"
-        aria-label="Partager sur Twitter"
-        type="button"
-        class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
-      >
-        <!-- href="https://twitter.com/intent/tweet?text=La+m%C3%A9diath%C3%A8que+Toki-Toki&amp;url=https%3A%2F%2Fcarte.bidarttourisme.com%2Fcontent%2Fwp_poi%2Ftoki-toki%2F%3Fsite%3Dteritorio&amp;via=carte.bidarttourisme.com" -->
-        <font-awesome-icon
-          :icon="['fab', 'twitter']"
-          :style="{ color: color }"
-        />
-      </button>
-    </li>
-    <li>
-      <button
-        v-if="title && href"
-        aria-label="Partager sur WhatsApp"
-        type="button"
-        class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
-      >
-        <!-- href="https://api.whatsapp.com/send?text=La+m%C3%A9diath%C3%A8que+Toki-Toki https%3A%2F%2Fcarte.bidarttourisme.com%2Fcontent%2Fwp_poi%2Ftoki-toki%2F%3Fsite%3Dteritorio" -->
-        <font-awesome-icon
-          :icon="['fab', 'whatsapp']"
-          :style="{ color: color }"
-        />
-      </button>
-    </li>
-    <li>
-      <button
-        aria-label="Imprimer"
-        type="button"
-        class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
-      >
-        <font-awesome-icon icon="print" :style="{ color: color }" />
-      </button>
-    </li>
-    <li>
-      <button
-        v-if="href"
-        aria-label="Copier le lien"
-        type="button"
-        class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
-      >
-        <font-awesome-icon icon="link" :style="{ color: color }" />
-      </button>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li>
+        <a
+          v-if="href"
+          aria-label="Partager sur Facebook"
+          type="button"
+          class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
+          :href="shareFacebook"
+          target="_black"
+        >
+          <font-awesome-icon
+            :icon="['fab', 'facebook']"
+            :style="{ color: color }"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          v-if="title && href"
+          aria-label="Partager sur Twitter"
+          type="button"
+          class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
+          :href="shareTwitter"
+          target="_black"
+        >
+          <font-awesome-icon
+            :icon="['fab', 'twitter']"
+            :style="{ color: color }"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          v-if="title && href"
+          aria-label="Partager sur WhatsApp"
+          type="button"
+          class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
+          :href="shareWhatsApp"
+          target="_black"
+        >
+          <font-awesome-icon
+            :icon="['fab', 'whatsapp']"
+            :style="{ color: color }"
+          />
+        </a>
+      </li>
+      <li>
+        <button
+          aria-label="Imprimer"
+          type="button"
+          class="text-sm text-gray-800 bg-white rounded-full shadow-md outline-none w-8 h-8 focus:outline-none hover:bg-gray-100 focus-visible:bg-gray-100 flex-shrink-0"
+          @click="print"
+        >
+          <font-awesome-icon icon="print" :style="{ color: color }" />
+        </button>
+      </li>
+      <li>
+        <button
+          v-if="href"
+          aria-label="Copier le lien"
+          type="button"
+          class="text-sm text-gray-800bg-whiterounded-fullshadow-mdoutline-nonew-8h-8focus:outline-nonehover:bg-gray-100focus-visible:bg-gray-100flex-shrink-0"
+          @click="shareUrl"
+        >
+          <font-awesome-icon icon="link" :style="{ color: color }" />
+        </button>
+      </li>
+    </ul>
+    <ShareLinkModal
+      title="Partager le lien"
+      :link="shareLink"
+      @close="shareLink = null"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import ShareLinkModal from '@/components/ShareLinkModal.vue'
+
 export default Vue.extend({
+  components: {
+    ShareLinkModal,
+  },
+
   props: {
     title: {
       type: String,
@@ -80,6 +98,49 @@ export default Vue.extend({
     color: {
       type: String,
       required: true,
+    },
+  },
+
+  data(): {
+    shareFacebook: string | null
+    shareTwitter: string | null
+    shareWhatsApp: string | null
+    shareLink: string | null
+  } {
+    return {
+      shareFacebook: null,
+      shareTwitter: null,
+      shareWhatsApp: null,
+      shareLink: null,
+    }
+  },
+
+  mounted() {
+    this.shareFacebook =
+      'https://www.facebook.com/share.php?' +
+      new URLSearchParams({
+        u: window.location.href,
+      }).toString()
+
+    this.shareTwitter =
+      'https://twitter.com/intent/tweet?' +
+      new URLSearchParams({
+        text: `${this.title}\n${window.location.href}`,
+      }).toString()
+
+    this.shareWhatsApp =
+      'https://api.whatsapp.com/send?' +
+      new URLSearchParams({
+        text: `${this.title}\n${window.location.href}`,
+      }).toString()
+  },
+
+  methods: {
+    print() {
+      window.print()
+    },
+    shareUrl() {
+      this.shareLink = window.location.href
     },
   },
 })
