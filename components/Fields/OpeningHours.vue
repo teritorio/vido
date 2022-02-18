d357ef55b54cfe4f0312c08f5ced2c8891ad0ade
joselegitan <guiguimed@hotmail.fr>
2022-02-19 00:25:29 +0100

    teritorio/vido-private#589 feat: add openingHours schedule (teritorio/vido-private#603)
    * feat: add openingHours schedule
    
    * fix: add missing :key
    
    Co-authored-by: Guillaume Schoder <g.schoder@leosidea.com>
    Co-authored-by: Frédéric Rodrigo <fred.rodrigo@gmail.com>

--- components/Fields/OpeningHours.vue
+++ components/Fields/OpeningHours.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <span
+      v-if="format"
       :class="[
         (format.includes($tc('openingHours.opened')) ||
           format === $tc('openingHours.24_7')) &&
@@ -11,7 +12,9 @@
       {{ format }}
     </span>
     <ul v-if="details">
-      <li>DETAILS TODO</li>
+      <li v-for="(timeline, i) in schedule" :key="`timeline_${i}`" class="mt-1">
+        {{ timeline }}
+      </li>
     </ul>
   </div>
 </template>
@@ -31,9 +34,21 @@ export default Vue.extend({
       default: false,
     },
   },
-
+  data() {
+    return {
+      days: [
+        this.$tc('openingHours.sunday'),
+        this.$tc('openingHours.monday'),
+        this.$tc('openingHours.tuesday'),
+        this.$tc('openingHours.wednesday'),
+        this.$tc('openingHours.thursday'),
+        this.$tc('openingHours.friday'),
+        this.$tc('openingHours.saturday'),
+      ],
+    }
+  },
   computed: {
-    format(): string | string[] | null {
+    format(): string | null {
       try {
         const oh = new OpeningHours(this.openingHours)
         const nextchange = oh.getNextChange()
@@ -51,30 +66,59 @@ export default Vue.extend({
         const todayDay = today.getDay()
         const day = nextChange.getDay()
 
-        const days = [
-          this.$tc('openingHours.sunday'),
-          this.$tc('openingHours.monday'),
-          this.$tc('openingHours.tuesday'),
-          this.$tc('openingHours.wednesday'),
-          this.$tc('openingHours.thursday'),
-          this.$tc('openingHours.friday'),
-          this.$tc('openingHours.saturday'),
-        ]
-
         const openTrad =
           todayDay === day
             ? `${this.$tc('openingHours.openAt')} ${this.displayTime(
                 nextchange
               )}`
-            : `${this.$tc('openingHours.open')} ${days[day]} ${this.displayTime(
-                nextchange
-              )}`
+            : `${this.$tc('openingHours.open')} ${
+                this.days[day]
+              } ${this.displayTime(nextchange)}`
 
         return `${this.$tc('openingHours.closed')} - ${openTrad}`
       } catch (e) {
         // eslint-disable-next-line no-console
         console.log('Vido error:', e)
         return null
+      }
+    },
+    schedule(): string[] {
+      try {
+        const oh = new OpeningHours(this.openingHours)
+
+        const from = new Date()
+        from.setDate(from.getDate() + ((7 - from.getDay()) % 7 || 7))
+
+        const to = new Date(from)
+        to.setDate(to.getDate() + 7)
+
+        const intervals = oh.getOpenIntervals(from, to)
+        const sortedIntervals: string[] = []
+
+        intervals.forEach((interval, i) => {
+          if (
+            i > 0 &&
+            interval[0].getDate() === intervals[i - 1][0].getDate()
+          ) {
+            sortedIntervals[
+              sortedIntervals.length - 1
+            ] += ` / ${this.displayTime(interval[0])}-${this.displayTime(
+              interval[1]
+            )}`
+          } else {
+            sortedIntervals.push(
+              `${this.days[interval[0].getDay()]} ${this.displayTime(
+                interval[0]
+              )}-${this.displayTime(interval[1])}`
+            )
+          }
+        })
+
+        return sortedIntervals
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log('Vido error:', e)
+        return []
       }
     },
   },
