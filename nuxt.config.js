a7dc76cce4c4d4bc568c33d6c57395eb7eb47252
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-02-23 15:41:32 +0100

    fix: remove not used nuxt/wpa deps

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,8 +97,6 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
     '@nuxtjs/i18n',
