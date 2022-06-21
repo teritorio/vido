b6183b55ce5cd07f289dbd140947e9c34c4e9fee
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-06-21 11:42:24 +0200

    feat: not fail safe missing host config

--- plugins/vido-config.ts
+++ plugins/vido-config.ts
@@ -5,18 +5,21 @@ import { VidoConfig } from '@/utils/types-config'
 import vidos from '@/vidos.config'
 
 export function vidoConfig(req: createServer.IncomingMessage): VidoConfig {
-  let host: string | undefined
+  let host: string
   if (process.server) {
-    host = (req.headers['x-forwarded-host'] as string) || req.headers.host
+    const hostHeader =
+      (req.headers['x-forwarded-host'] as string) || req.headers.host
+    if (!hostHeader) {
+      throw new Error('No header "Host" nor "x-forwarded-host"')
+    } else {
+      host = hostHeader
+    }
   } else {
     host = window.location.host
   }
   host = host?.split(':')[0]
-  if (host && !(host in vidos)) {
-    host = undefined
-  }
-  if (!host) {
-    host = Object.keys(vidos)[0]
+  if (!(host in vidos)) {
+    throw new Error(`Not configured host "${host}"`)
   }
   return {
     ...vidos[host],
