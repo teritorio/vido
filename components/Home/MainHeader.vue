95c526033bbbb5f26c0f33cc18d2f64af476fa9a
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-09-03 21:06:34 +0200

    refactor: refactoring menu block style

--- components/Home/MainHeader.vue
+++ components/Home/MainHeader.vue
@@ -1,47 +1,47 @@
 <template>
-  <aside
-    class="hidden md:flex flex-col absolute bottom-0 overflow-y-hidden pointer-events-auto sm:relative inset-auto h-max max-h-full box-border space-y-4 p-2 box-border"
-  >
-    <div
-      class="items-center justify-between hidden md:flex rounded-xl shadow-md px-5 py-4 bg-white"
+  <MenuBlock>
+    <a
+      :href="mainUrl"
+      rel="noopener noreferrer"
+      :aria-label="siteName"
+      :title="siteName"
+      target="_blank"
     >
-      <a
-        :href="mainUrl"
-        rel="noopener noreferrer"
-        :aria-label="siteName"
-        :title="siteName"
-        target="_blank"
-      >
-        <img :src="logoUrl" class="w-auto h-auto max-w-2xl max-h-16" />
-      </a>
+      <img :src="logoUrl" class="w-auto h-auto max-w-2xl max-h-16" />
+    </a>
 
-      <button
-        type="button"
-        class="flex-grow relative pointer-events-auto w-full ml-6"
-        @click="onSearchClick"
-      >
-        <div class="relative w-full">
-          <input
-            ref="search"
-            value=""
-            class="w-full px-5 py-3 font-medium text-zinc-700 placeholder-zinc-500 bg-zinc-100 border-none rounded-full outline-none appearance-none focus:outline-none focus:ring focus:ring-zinc-300"
-            :placeholder="$tc('headerMenu.search')"
-            type="text"
-            @focus="$tracking({ type: 'search' })"
-          />
-          <div class="absolute inset-y-0 right-0 px-5 py-3 text-zinc-800">
-            <font-awesome-icon icon="search" />
-          </div>
+    <button
+      type="button"
+      class="flex-grow relative pointer-events-auto w-full ml-6"
+      @click="onSearchClick"
+    >
+      <div class="relative w-full">
+        <input
+          ref="search"
+          value=""
+          class="w-full px-5 py-3 font-medium text-zinc-700 placeholder-zinc-500 bg-zinc-100 border-none rounded-full outline-none appearance-none focus:outline-none focus:ring focus:ring-zinc-300"
+          :placeholder="$tc('headerMenu.search')"
+          type="text"
+          @focus="$tracking({ type: 'search' })"
+        />
+        <div class="absolute inset-y-0 right-0 px-5 py-3 text-zinc-800">
+          <font-awesome-icon icon="search" />
         </div>
-      </button>
-    </div>
-  </aside>
+      </div>
+    </button>
+  </MenuBlock>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import MenuBlock from '~/components/Home/MenuBlock.vue'
+
 export default Vue.extend({
+  components: {
+    MenuBlock,
+  },
+
   props: {
     logoUrl: {
       type: String,
---
+++ components/Home/MenuBlock.vue
@@ -0,0 +1,27 @@
+<template>
+  <aside
+    class="hidden md:flex flex-col absolute bottom-0 overflow-y-hidden pointer-events-auto sm:relative inset-auto h-max max-h-full box-border space-y-4 p-2 box-border"
+  >
+    <div
+      :class="[
+        'items-center justify-between hidden md:block rounded-xl shadow-md px-5 py-4',
+        extraClassTextBackground,
+      ]"
+    >
+      <slot />
+    </div>
+  </aside>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    extraClassTextBackground: {
+      type: String,
+      default: 'bg-white',
+    },
+  },
+})
+</script>
