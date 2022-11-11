27b795aefc630c2b38851c63e5cbda9ee5a9358e
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-11-11 16:58:59 +0100

    chore: add cypress-terminal-report to help debug from github actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      require('cypress-terminal-report/src/installLogsPrinter')(on)
+    },
   },
 })
