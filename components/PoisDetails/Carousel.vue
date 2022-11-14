156c58bd053e31c9ab09ca51072acbbb1e8254e8
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-11-14 16:44:54 +0100

    fix: add css and vue-agile workaround  #13

--- components/PoisDetails/Carousel.vue
+++ components/PoisDetails/Carousel.vue
@@ -5,7 +5,7 @@
   <agile
     v-else-if="images.length > 1"
     class="margin"
-    :dot="true"
+    :dots="dots"
     :nav-buttons="false"
   >
     <div v-for="(image, i) in images" :key="i" class="slide">
@@ -29,11 +29,66 @@ export default Vue.extend({
       required: true,
     },
   },
+
+  data(): {
+    dots: boolean
+  } {
+    return {
+      dots: false,
+    }
+  },
+
+  mounted() {
+    // Workarround SSR issue.
+    // https://github.com/lukaszflorczak/vue-agile/issues/163
+    this.dots = true
+  },
 })
 </script>
 
 <style lang="css" scoped>
 .margin {
   margin-bottom: 60px;
+}
+</style>
+
+<style lang="css">
+.agile__dots {
+  bottom: 15px;
+  left: 50%;
+  position: absolute;
+  transform: translateX(-50%);
+}
+
+.agile__dot {
+  margin: 0 5px;
+}
+
+.agile__dot button {
+  background-color: transparent;
+  border: 1px solid #fff;
+  border-radius: 50%;
+  cursor: pointer;
+  display: block;
+  height: 15px;
+  font-size: 0;
+  line-height: 0;
+  margin: 0;
+  padding: 0;
+  transition-duration: 0.3s;
+  width: 15px;
+}
+
+.agile__dot--current button,
+.agile__dot:hover button {
+  background-color: #fff;
+}
+
+.slide {
+  display: block;
+  height: 500px;
+  -o-object-fit: cover;
+  object-fit: cover;
+  width: 100%;
 }
 </style>
