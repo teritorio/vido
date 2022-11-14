957f9c04580433b61dcafd7ce9cc4b83b473eec8
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-11-14 17:43:50 +0100

    fix: revert filed header h3

--- components/UI/FieldsHeader.vue
+++ components/UI/FieldsHeader.vue
@@ -2,9 +2,6 @@
   <h2 v-if="recursionLevel === 0">
     <slot />
   </h2>
-  <h3 v-else-if="recursionLevel === 1">
-    <slot />
-  </h3>
   <span v-else><slot />{{ $tc('ponctuation.colon') }}</span>
 </template>
 
