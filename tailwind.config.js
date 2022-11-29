4fc9dbea605480d02e4be1b81c921972327c312d
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-11-29 16:34:37 +0100

    fix: clean tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,10 @@ module.exports = {
         'screen-3/5': '60vh',
       }),
       maxHeight: () => ({
-        'screen-1/4': '25vh',
-        'screen-1/3': '31.3vh',
-        'screen-1/2': '50vh',
-        'screen-2/5': '40vh',
         'screen-3/5': '60vh',
         'screen-4/6': '65vh',
       }),
       minHeight: () => ({
-        'screen-1/4': '25vh',
-        'screen-1/2': '50vh',
         'screen-3/5': '60vh',
       }),
       flexBasis: {
