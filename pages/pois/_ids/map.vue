0fbc2cd761b9bd8b9c3e74fa01273cebc852dcc8
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-11-29 11:15:40 +0100

    fix: make font usage more standard

--- pages/pois/_ids/map.vue
+++ pages/pois/_ids/map.vue
@@ -105,29 +105,10 @@ html {
   @apply h-full w-full box-border overflow-hidden overscroll-none;
 }
 
-html,
-.mapboxgl-map {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  @apply text-zinc-900;
-}
-
 body,
 #__nuxt,
 #__layout {
   @apply h-full w-full overflow-hidden overscroll-none;
-}
-
-*,
-*::before,
-*::after {
-  @apply m-0 box-border;
 }
 
 #map-container {
