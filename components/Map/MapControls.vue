a53529cdb51846fc7b36b2f65146519f924c8f00
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-12-01 16:32:32 +0100

    fix: make maplibre css rule more specifics

--- components/Map/MapControls.vue
+++ components/Map/MapControls.vue
@@ -57,49 +57,49 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-.mapboxgl-ctrl-group,
-.maplibre-ctrl-group {
+#map .mapboxgl-ctrl-group,
+#map .maplibre-ctrl-group {
   @apply mb-4;
 
   background: none;
 }
 
-.mapboxgl-ctrl-group:not(:empty),
-.maplibregl-ctrl-group:not(:empty) {
+#map .mapboxgl-ctrl-group:not(:empty),
+#map .maplibregl-ctrl-group:not(:empty) {
   box-shadow: none;
 }
 
-.mapboxgl-ctrl-group > button,
-.maplibregl-ctrl-group > button,
-.mapboxgl-ctrl-group > button:not(:disabled),
-.maplibregl-ctrl-group > button:not(:disabled) {
+#map .mapboxgl-ctrl-group > button,
+#map .maplibregl-ctrl-group > button,
+#map .mapboxgl-ctrl-group > button:not(:disabled),
+#map .maplibregl-ctrl-group > button:not(:disabled) {
   @apply text-sm font-bold text-zinc-800 bg-white rounded-full shadow-md outline-none w-11 h-11;
   @apply focus:rounded-full focus:shadow-md focus:outline-none focus-visible:bg-zinc-100;
   @apply hover:bg-zinc-100;
 }
 
-.mapboxgl-ctrl-group > button.mapboxgl-ctrl-active,
-.maplibregl-ctrl-group > button.maplibregl-ctrl-active,
-.mapboxgl-ctrl-group > button.mapboxgl-ctrlactive:not(:disabled),
-.maplibregl-ctrl-group > button.maplibregl-ctrl-active:not(:disabled) {
+#map .mapboxgl-ctrl-group > button.mapboxgl-ctrl-active,
+#map .maplibregl-ctrl-group > button.maplibregl-ctrl-active,
+#map .mapboxgl-ctrl-group > button.mapboxgl-ctrlactive:not(:disabled),
+#map .maplibregl-ctrl-group > button.maplibregl-ctrl-active:not(:disabled) {
   @apply bg-blue-500 text-white hover:bg-blue-400 focus-visible:bg-blue-400;
 }
 
-.mapboxgl-ctrl-top-right,
-.maplibregl-ctrl-top-right {
+#map .mapboxgl-ctrl-top-right,
+#map .maplibregl-ctrl-top-right {
   @apply flex flex-col justify-center inset-y-3;
 }
 
-.mapboxgl-ctrl-attrib,
-.maplibregl-ctrl-attrib {
+#map .mapboxgl-ctrl-attrib,
+#map .maplibregl-ctrl-attrib {
   font-size: 0.75rem;
   line-height: 1rem;
 }
 
-.mapboxgl-ctrl-group > button.mapboxgl-ctrl-geolocate,
-.maplibregl-ctrl-group > button.maplibregl-ctrl-geolocate,
-.mapboxgl-ctrl-group > button.mapboxgl-ctrl-geolocate:not(:disabled),
-.maplibregl-ctrl-group > button.maplibregl-ctrl-geolocate:not(:disabled) {
+#map .mapboxgl-ctrl-group > button.mapboxgl-ctrl-geolocate,
+#map .maplibregl-ctrl-group > button.maplibregl-ctrl-geolocate,
+#map .mapboxgl-ctrl-group > button.mapboxgl-ctrl-geolocate:not(:disabled),
+#map .maplibregl-ctrl-group > button.maplibregl-ctrl-geolocate:not(:disabled) {
   @apply md:hidden;
 }
 </style>
