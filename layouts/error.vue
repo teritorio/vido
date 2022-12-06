9ab25d0f06bb73606cdbc02060df2d4c25e98c30
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-12-06 14:41:47 +0100

    feat: add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <h1 v-if="error.statusCode === 404">404<br />Page not found</h1>
+      <h1 v-else>An error occurred - {{ error.statusCode }}</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'error-layout',
+
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
