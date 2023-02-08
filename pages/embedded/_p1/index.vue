81d1e2a525ec357dbb8d60da1a6be1b520d0b1f4
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-02-08 20:26:47 +0100

    fix: add missing embedded files

---
+++ pages/embedded/_p1/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <span />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  validate({ params }) {
+    return params.p1 === undefined || /^[0-9,]+$/.test(params.p1)
+  },
+})
+</script>
