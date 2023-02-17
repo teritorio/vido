271e48ecc6916f60fa2a27303c41e3b3a1fda733
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-02-17 16:56:27 +0100

    feat: port nuxt plugin to Nuxt3

--- plugins/pinia-shared-state.ts
+++ plugins/pinia-shared-state.ts
@@ -1,13 +1,15 @@
 import { Plugin } from '@nuxt/types'
 import { PiniaSharedState } from 'pinia-shared-state'
 
+import { defineNuxtPlugin } from '#app/nuxt'
+
 // @ts-ignore
-const piniaSharedState: Plugin = ({ $pinia }) => {
+const piniaSharedState: Plugin = defineNuxtPlugin(({ $pinia }) => {
   $pinia.use(
     PiniaSharedState({
       enable: false,
     })
   )
-}
+})
 
 export default piniaSharedState
