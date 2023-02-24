73d9980b5d8435d96f7dbe1db84161a427b8c0b5
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-02-24 18:37:03 +0100

    fix: fix default config load

--- lib/config.ts
+++ lib/config.ts
@@ -2,8 +2,6 @@ import fs from 'fs'
 
 import { VidosConfig } from '~/utils/types-config'
 
-export const vidos: VidosConfig = !process.env.CONFIG
-  ? {}
-  : JSON.parse(
-      fs.readFileSync(process.env.CONFIG || 'vidos-config.json').toString()
-    )
+export const vidos: VidosConfig = JSON.parse(
+  fs.readFileSync(process.env.CONFIG || 'vidos-config.json').toString()
+)
