ad1c18e5bfdbf3c0f47fef708c82904002d65111
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-02-25 17:52:55 +0100

    fix: fix regretion on URL param validation

--- pages/index/_p1.vue
+++ pages/index/_p1.vue
@@ -7,7 +7,7 @@ import Vue from 'vue'
 
 export default Vue.extend({
   validate({ params }) {
-    return params.p1 === undefined || /^[0-9,]+$/.test(params.p1)
+    return params.p1 === undefined || /^[-_:a-zA-Z0-9,]+$/.test(params.p1)
   },
 })
 </script>
