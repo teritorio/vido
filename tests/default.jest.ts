b3d4fcf70b78158395e2eb27fbdae024ff5e92bc
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-03-06 13:58:31 +0100

    fix: remove vuex from test

--- tests/default.jest.ts
+++ tests/default.jest.ts
@@ -1,11 +1,8 @@
 import { config } from '@vue/test-utils'
 import Vue from 'vue'
-import Vuex from 'vuex'
 
 import settings from '~/cypress/fixtures/teritorio/references/settings.json'
 import fr from '~/locales/fr.js'
-
-Vue.use(Vuex)
 
 config.mocks['$t'] = (k: string) =>
   // @ts-ignore
