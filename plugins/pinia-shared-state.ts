7ccbbfcdfc9b93c74132b7d94dc04d9be34b9982
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-03-09 16:14:34 +0100

    fix: ignore typing error

--- plugins/pinia-shared-state.ts
+++ plugins/pinia-shared-state.ts
@@ -1,6 +1,7 @@
 import { Plugin } from '@nuxt/types'
 import { PiniaSharedState } from 'pinia-shared-state'
 
+// @ts-ignore
 const piniaSharedState: Plugin = ({ $pinia }) => {
   $pinia.use(
     PiniaSharedState({
