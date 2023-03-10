7b2d49d6d7b4e7a74e9376038539869574461704
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-03-10 10:55:35 +0100

    chore: update country-flag-icons

---
+++ components/UI/VFlag.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { hasFlag } from 'country-flag-icons'
+import { FR, GB, ES } from 'country-flag-icons/string/3x2'
+import { defineComponent, h, ref, watch } from 'vue'
+
+export default defineComponent({
+  name: 'VFlag',
+
+  props: {
+    flag: {
+      type: String,
+      required: true,
+      validator: (value: string) => {
+        return hasFlag(value)
+      },
+    },
+  },
+
+  setup(props) {
+    const svgFlag = ref(null)
+    const svgs = { FR, GB, ES }
+    watch(
+      () => props.flag,
+      async (newFlag, oldFlag) => {
+        if (newFlag !== oldFlag) {
+          // @ts-ignore
+          svgFlag.value = svgs[newFlag]
+        }
+      },
+      { immediate: true }
+    )
+    return () => h('div', { innerHTML: svgFlag.value })
+  },
+})
+</script>
