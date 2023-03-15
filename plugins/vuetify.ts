f7ebf6bef8f349a01ade01d63af3d1fc91d89bd9
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-03-15 17:21:33 +0100

    fix: use fa in vuetify

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,8 +1,18 @@
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import 'vuetify/styles'
+
 import { defineNuxtPlugin } from '#app/nuxt'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
     ssr: true,
   })
 
