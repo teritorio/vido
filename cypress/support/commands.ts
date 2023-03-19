19f6a812c9c0d51316684ee1ca3f4b380f76b4c0
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-03-19 17:09:29 +0100

    test: cypress mock, replace nock by mws

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -4,26 +4,14 @@
 declare global {
   namespace Cypress {
     interface Chainable {
-      mockSSR: (options?: any) => void
       clearSSRMocks: () => void
     }
   }
 }
 
 // @ts-ignore
-Cypress.Commands.add('mockSSR', (payload) => {
-  // @ts-ignore
-  cy.request('POST', '/__cypress_server_mock', payload)
-})
-
-// @ts-ignore
 Cypress.Commands.add('clearSSRMocks', () => {
   cy.request('/__cypress_clear_mocks')
-})
-
-beforeEach(() => {
-  // @ts-ignore
-  cy.clearSSRMocks()
 })
 
 after(() => {
