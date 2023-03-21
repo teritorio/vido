fb41a767b311e15cc0d2bbe839ef8c5b6afefee7
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-03-21 13:05:36 +0100

    test: fix 'some' issues with jest

--- jest.config.ts
+++ jest.config.ts
@@ -5,13 +5,12 @@ const config: Config.InitialOptions = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js',
   },
-  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  moduleFileExtensions: ['ts', 'js', 'vue'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    '.*\\.(vue)$': '@vue/vue3-jest',
     '^.+\\.(aac|eot|gif|jpeg|jpg|m4a|mp3|mp4|oga|otf|pdf|png|svg|ttf|wav|webm|webp|woff|woff2)$':
       '<rootDir>/__mocks__/file-mock.js',
   },
