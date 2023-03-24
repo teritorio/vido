beb46e8be8bf9984b840e56b1f951aa75fa8294d
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-03-24 22:00:18 +0100

    test: fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,7 @@ const config: Config.InitialOptions = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': '@vue/vue3-jest',
+    '.*\\.vue$': '@vue/vue3-jest',
     '^.+\\.(aac|eot|gif|jpeg|jpg|m4a|mp3|mp4|oga|otf|pdf|png|svg|ttf|wav|webm|webp|woff|woff2)$':
       '<rootDir>/__mocks__/file-mock.js',
   },
@@ -24,6 +24,7 @@ const config: Config.InitialOptions = {
   testEnvironmentOptions: {
     customExportConditions: ['node', 'node-addons'],
   },
+  testPathIgnorePatterns: ['/node_modules/'],
 }
 
 export default config
