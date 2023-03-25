95560a4acb61e02ba1ee81c1782b0bbb260dfd75
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-03-25 16:13:22 +0100

    fix: fix typing in OpeningHours.test.ts

--- components/Fields/OpeningHours.test.ts
+++ components/Fields/OpeningHours.test.ts
@@ -1,6 +1,6 @@
 import { expect } from '@jest/globals'
-import { shallowMount } from '@vue/test-utils'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import OpeningHours from '~/components/Fields/OpeningHours.vue'
 import { PropertyTranslationsContextEnum } from '~/plugins/property-translations'
@@ -15,41 +15,41 @@ beforeEach(() => {
 })
 
 function factory(props = {}) {
-  return shallowMount(OpeningHours, {
-    propsData: {
-      context: PropertyTranslationsContextEnum.Default,
-      tagKey: 'opening_hours',
-      openingHours: '24/7',
-      baseDate: new Date('2022-01-02 11:00:00'), // Sunday
-      ...props,
-    },
-  })
+  const el: HTMLElement = document.createElement('div')
+  createApp(OpeningHours, {
+    context: PropertyTranslationsContextEnum.Default,
+    tagKey: 'opening_hours',
+    openingHours: '24/7',
+    baseDate: new Date('2022-01-02 11:00:00'), // Sunday
+    ...props,
+  }).mount(el)
+  return el
 }
 
 test('opening_hours', () => {
   let wrapper
   wrapper = factory({ openingHours: 'Su 00:00-24:00' })
-  expect(wrapper.find('#opened').exists()).toBeTruthy()
+  expect(wrapper.querySelector('#opened')).toBeTruthy()
 
   wrapper = factory({ openingHours: 'Sa 00:00-24:00' })
-  expect(wrapper.find('#openAt').exists()).toBeTruthy()
+  expect(wrapper.querySelector('#openAt')).toBeTruthy()
 
   wrapper = factory({ openingHours: 'k; fjlk-gj; lrjglkregm' })
-  expect(wrapper.find('#_24_7').exists()).toBeFalsy()
-  expect(wrapper.find('#opened').exists()).toBeFalsy()
-  expect(wrapper.find('#openAt').exists()).toBeFalsy()
+  expect(wrapper.querySelector('#_24_7')).toBeFalsy()
+  expect(wrapper.querySelector('#opened')).toBeFalsy()
+  expect(wrapper.querySelector('#openAt')).toBeFalsy()
 })
 
 test('collection_times', () => {
   let wrapper
   wrapper = factory({ tagKey: 'collection_times', openingHours: 'Su 00:00' })
-  expect(wrapper.find('#next').exists()).toBeTruthy()
+  expect(wrapper.querySelector('#next')).toBeTruthy()
 
   wrapper = factory({
     tagKey: 'collection_times',
     openingHours: 'k; fjlk-gj; lrjglkregm',
   })
-  expect(wrapper.find('#next').exists()).toBeFalsy()
+  expect(wrapper.querySelector('#next')).toBeFalsy()
 })
 
 test('pretty', () => {
@@ -57,15 +57,15 @@ test('pretty', () => {
   wrapper = factory({
     openingHours: 'Tu-Sa 07:00-12:30,13:30-19:00; Su 07:30-12:15',
   })
-  expect(wrapper.find('ul > li:nth-child(1)').html()).toEqual(
+  expect(wrapper.querySelector('ul > li:nth-child(1)')?.innerHTML).toEqual(
     '<li>Mar.-sam. 07:00-12:30,13:30-19:00</li>'
   )
-  expect(wrapper.find('ul > li:nth-child(2)').html()).toEqual(
+  expect(wrapper.querySelector('ul > li:nth-child(2)')?.innerHTML).toEqual(
     '<li>Dim. 07:30-12:15</li>'
   )
 
   wrapper = factory({ openingHours: 'Apr-Oct: Fr-Su 10:00-18:00' })
-  expect(wrapper.find('ul > li:nth-child(1)').html()).toEqual(`<li>
+  expect(wrapper.querySelector('ul > li:nth-child(1)')?.innerHTML).toEqual(`<li>
   Avr.-oct.
   <ul>
     <li>Ven.-dim. 10:00-18:00</li>
@@ -75,10 +75,10 @@ test('pretty', () => {
   wrapper = factory({
     openingHours: 'Tu-Sa 07:00-12:30,13:30-19:00; Su,PH 07:30-12:15',
   })
-  expect(wrapper.find('ul > li:nth-child(1)').html()).toEqual(
+  expect(wrapper.querySelector('ul > li:nth-child(1)')?.innerHTML).toEqual(
     '<li>Mar.-sam. 07:00-12:30,13:30-19:00</li>'
   )
-  expect(wrapper.find('ul > li:nth-child(2)').html()).toEqual(
+  expect(wrapper.querySelector('ul > li:nth-child(2)')?.innerHTML).toEqual(
     '<li>Dim.,PH 07:30-12:15</li>'
   )
 })
