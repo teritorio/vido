4874914ef3df7b67c96088e80a9221b7340317e7
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-04-08 15:56:30 +0200

    fix: fix overlay click

--- components/MainMap/FavoritesOverlay.vue
+++ components/MainMap/FavoritesOverlay.vue
@@ -1,8 +1,8 @@
 <template>
   <div
     v-touch="onOverlayClick"
-    :click="onOverlayClick"
     class="tw-fixed tw-inset-0 tw-flex tw-items-center tw-justify-center tw-bg-black tw-bg-opacity-80 tw-z-20 tw-cursor-pointer"
+    @click="onOverlayClick"
   >
     <p class="tw-p-8 tw-max-w-sm tw-text-center tw-text-white">
       {{ $t('favorites.noFavs') }}
