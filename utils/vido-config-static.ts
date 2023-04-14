a1358f5f7843c4cd8f3eb1321b889979d10f8905
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-04-14 18:31:17 +0200

    feat: load vidos config at runtime

--- utils/vido-config-static.ts
+++ utils/vido-config-static.ts
@@ -1,19 +1,19 @@
 import { VidosConfig } from '~/utils/types-config'
 
-export function configuredApi(vidos: VidosConfig): string[] {
+export function configuredApi(vidos: () => VidosConfig): string[] {
   return [
     ...new Set(
-      Object.values(vidos)
+      Object.values(vidos())
         .map((vido) => vido.API_ENDPOINT || [])
         .flat()
     ),
   ].map((url) => new URL(url).hostname)
 }
 
-export function configuredImageProxy(vidos: VidosConfig): string[] {
+export function configuredImageProxy(vidos: () => VidosConfig): string[] {
   return [
     ...new Set(
-      Object.values(vidos)
+      Object.values(vidos())
         .map((vido) => vido.IMAGE_PROXY || [])
         .flat()
     ),
