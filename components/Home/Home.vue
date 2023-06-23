275298520d0a4796fe933874e6b07ae8d7eea2b3
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-06-23 13:45:23 +0200

    fix: fix main menu scrool #80

--- components/Home/Home.vue
+++ components/Home/Home.vue
@@ -50,7 +50,7 @@
           mode="out-in"
           :class="[
             'tw-hidden md:tw-block',
-            'flex-none tw-max-w-md tw-overflow-scroll flex-shrink-0',
+            'flex-none tw-max-w-md tw-overflow-y-auto tw-overflow-x-clip flex-shrink-0',
           ]"
         >
           <MenuBlock
