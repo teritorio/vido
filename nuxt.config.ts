ffc2cd714f939e364fdafdcacc7866ac930ee7f6
Noé Viricel <noe.viricel@wazo-lab.io>
2024-01-11 20:52:49 +0100

    chore: clean nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,70 @@ import vuetify from 'vite-plugin-vuetify'
 import { vidos } from './lib/config'
 import { configuredApi, configuredImageProxy } from './utils/vido-config-static'
 
+// Build Configuration (https://nuxt.com/docs/api/nuxt-config)
 export default defineNuxtConfig({
+  build: {
+    transpile: [
+      process.env.NODE_ENV === 'production' ? 'maplibre-gl' : '',
+      'iron-webcrypto',
+      'punycode',
+      'pinia',
+      'vuetify',
+      'date-fns',
+      /lodash.*/,
+      '@fortawesome/vue-fontawesome',
+    ],
+  },
+  buildModules: ['@nuxtjs/stylelint-module'],
+  components: false,
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+  gtm: {
+    pageTracking: false,
+  },
+  head: {
+    htmlAttrs: {
+      lang: 'fr',
+    },
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    title: '@teritorio/vido',
+  },
+  i18n: {
+    defaultLocale: 'en',
+    detectBrowserLanguage: {
+      useCookie: false,
+    },
+    langDir: 'locales/',
+    locales: [
+      { code: 'en', name: 'English', flag: 'GB', iso: 'en-US', file: 'en.js' },
+      { code: 'es', name: 'Español', flag: 'ES', iso: 'es-ES', file: 'es.js' },
+      { code: 'fr', name: 'Français', flag: 'FR', iso: 'fr-FR', file: 'fr.js' },
+    ],
+    strategy: 'no_prefix',
+  },
+  image: {
+    domains: [...configuredApi(vidos), ...configuredImageProxy(vidos)],
+  },
+  imports: {
+    autoImport: false,
+  },
+  loadingIndicator: false,
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxt/image-edge',
+    // process.env.SENTRY_DSN ? ['@nuxtjs/sentry'] : [],
+    '@pinia/nuxt',
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) =>
+        // @ts-ignore
+        config.plugins.push(vuetify())
+      )
+    },
+  ],
   runtimeConfig: {
     public: {
       // @ts-ignore
@@ -12,36 +75,6 @@ export default defineNuxtConfig({
       cypress: process.env.CYPRESS,
     },
   },
-
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    htmlAttrs: {
-      lang: 'fr',
-    },
-    title: '@teritorio/vido',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-
-  router: {
-    // prefetchLinks: false,
-  },
-
-  // @ts-ignore
-  loadingIndicator: false,
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
-
-  purgeCSS: {
-    whitelistPatterns: [/svg.*/, /fa.*/],
-  },
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '@/plugins/vido-config.ts',
     '@/plugins/settings.ts',
@@ -52,60 +85,31 @@ export default defineNuxtConfig({
     '@/plugins/property-translations.ts',
     { src: '@/plugins/pinia-shared-state.ts', mode: 'client' },
   ],
-
-  imports: {
-    autoImport: false,
-  },
-
-  components: false,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/i18n',
-    '@nuxt/image-edge',
-    ...(process.env.SENTRY_DSN ? ['@nuxtjs/sentry'] : []),
-    '@pinia/nuxt',
-    async (options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) =>
-        // @ts-ignore
-        config.plugins.push(vuetify())
-      )
-    },
-  ],
-
-  i18n: {
-    strategy: 'no_prefix',
-    // lazy: true, // lazy-load not working as expected https://i18n.nuxtjs.org/lazy-load-translations/
-    detectBrowserLanguage: {
-      useCookie: false,
-    },
-    locales: [
-      { code: 'en', name: 'English', flag: 'GB', iso: 'en-US', file: 'en.js' },
-      { code: 'es', name: 'Español', flag: 'ES', iso: 'es-ES', file: 'es.js' },
-      { code: 'fr', name: 'Français', flag: 'FR', iso: 'fr-FR', file: 'fr.js' },
-    ],
-    defaultLocale: 'en',
-    langDir: 'locales/',
-  },
-
-  image: {
-    domains: [...configuredApi(vidos), ...configuredImageProxy(vidos)],
-  },
-
-  sentry: {
-    dsn: process.env.SENTRY_DSN || '',
-    // https://sentry.nuxtjs.org/sentry/options
-    config: {
-      // https://docs.sentry.io/platforms/javascript/guides/vue/configuration/options/
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
   },
-
+  purgeCSS: {
+    whitelistPatterns: [/svg.*/, /fa.*/],
+  }, 
+  // sentry: {
+  //   dsn: process.env.SENTRY_DSN || '',
+  //   // https://sentry.nuxtjs.org/sentry/options
+  //   config: {
+  //     // https://docs.sentry.io/platforms/javascript/guides/vue/configuration/options/
+  //   },
+  // },
+  server: {
+    host: '0.0.0.0',
+  },
+  typescript: {
+    shim: false
+  },
+  vite: {
+    optimizeDeps: { exclude: ['fsevents'] },
+  },
   watchers: {
     chokidar: {
       // @ts-ignore
@@ -122,44 +126,5 @@ export default defineNuxtConfig({
       // @ts-ignore
       ignored: /\.git|\.yarn|cypress|.*\.story\.vue$|\.nuxt\/dist\/server\//,
     },
-  },
-
-  // Build Configuration (https://nuxt.com/docs/api/nuxt-config#build)
-  build: {
-    transpile: [
-      process.env.NODE_ENV === 'production' ? 'maplibre-gl' : '',
-      'iron-webcrypto',
-      'punycode',
-      'pinia',
-      'vuetify',
-      'date-fns',
-      /lodash.*/,
-      '@fortawesome/vue-fontawesome',
-    ],
-  },
-
-  typescript: {
-    // typeCheck: !!process.env.TYPESCRIPT_CHECK,
-  },
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  
-  vite: {
-    optimizeDeps: { exclude: ['fsevents'] },
-  },
-
-  // Server config (allow listening to local network)
-  server: {
-    host: '0.0.0.0',
-  },
-
-  // Google Tag Manager config
-  gtm: {
-    pageTracking: false,
-  },
+  }
 })
