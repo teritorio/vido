a239218016d8d47e2ce108d45228acee67f8eb21
Noé Viricel <noe.viricel@wazo-lab.io>
2024-01-12 03:22:16 +0100

    fix: improve i18n integration + fix hover on NavMenu #124

--- components/MainMap/NavMenu.vue
+++ components/MainMap/NavMenu.vue
@@ -1,45 +1,26 @@
 <template>
-  <section
-    v-if="entries.length + locales.length > 0"
-    class="tw-relative tw-z-40"
-  >
+  <section v-if="entries.length + locales.length > 0" class="tw-relative tw-z-40">
     <v-menu offset-y>
       <template #activator="{ props }">
-        <IconButton
-          :label="$t('navMenu.label')"
-          class="tw-w-11 tw-h-11 tw-pointer-events-auto"
-          v-bind="props"
-        >
+        <IconButton :label="$t('navMenu.label')" class="tw-w-11 tw-h-11 tw-pointer-events-auto" v-bind="props">
           <FontAwesomeIcon icon="cog" class="tw-text-zinc-800" size="lg" />
         </IconButton>
       </template>
 
       <v-list id="nav-menu-dropdown">
-        <v-list-item
-          v-for="entry in entries"
-          :key="entry.post_id"
-          class="tw-w-full tw-px-5 tw-py-3 hover:tw-bg-zinc-100"
-        >
+        <v-list-item v-for="entry in entries" :key="entry.post_id" class="tw-w-full tw-px-5 tw-py-3 hover:tw-bg-zinc-100">
           <v-list-item-title>
-            <ExternalLink
-              :href="entry.url"
-              rel="noopener noreferrer"
-              target="_blank"
-              @click="openLink(entry.title, entry.url)"
-            >
+            <ExternalLink :href="entry.url" rel="noopener noreferrer" target="_blank"
+              @click="openLink(entry.title, entry.url)">
               {{ entry.title }}
             </ExternalLink>
           </v-list-item-title>
         </v-list-item>
-        <v-divider v-if="Boolean(entries.length)"></v-divider>
-        <v-list-item
-          v-for="locale in locales"
-          :key="locale.code"
-          :class="[
-            'tw-w-full tw-px-5 tw-py-3 hover:tw-bg-zinc-100',
-            locale.code === $i18n.locale && 'bg-grey-lighten-2',
-          ]"
-        >
+        <v-divider v-if="Boolean(entries.length)" />
+        <v-list-item v-for="locale in locales" :key="locale.code" :class="[
+          'tw-w-full tw-px-5 tw-py-3 hover:tw-bg-zinc-100',
+          locale.code === $i18n.locale && 'bg-grey-lighten-2',
+        ]">
           <v-list-item-title>
             <a href="#" @click.prevent="setLocale(locale.code)">
               <VFlag :flag="locale.flag" class="flag" />
@@ -97,8 +78,8 @@ export default defineNuxtComponent({
   },
 
   methods: {
-    setLocale(locale: string) {
-      this.$i18n.locale = locale
+    async setLocale(locale: string) {
+      await this.$i18n.setLocale(locale)
       this.locale = locale
     },
     openLink(title: string, url: string) {
