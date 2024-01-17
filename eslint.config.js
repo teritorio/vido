3ffb3da47f8f8f49643579894434aa295bb5e325
Noé Viricel <noe.viricel@wazo-lab.io>
2024-01-17 17:17:13 +0100

    chore: update eslint config rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,12 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/no-unused-refs': 0,
+    },
+  },
+  {
     rules: {
       'node/prefer-global/process': 0,
     },
