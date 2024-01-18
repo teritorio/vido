06b5d1df1508775f38734060dc85fb87201c39bf
Noé Viricel <noe.viricel@wazo-lab.io>
2024-01-18 23:50:28 +0100

    chore: setup stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,16 +1,6 @@
-module.exports = {
+/** @type {import('stylelint').Config} */
+
+export default {
+  $schema: 'https://json.schemastore.org/stylelintrc.json',
   extends: ['stylelint-config-standard'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  plugins: ['stylelint-scss'],
-  rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true,
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        ignorePseudoClasses: ['deep'],
-      },
-    ],
-  },
 }
