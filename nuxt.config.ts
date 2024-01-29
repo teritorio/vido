289ec33da2006c6c33893645259d7685d0a5a4f3
Noé Viricel <noe.viricel@wazo-lab.io>
2024-01-29 18:11:22 +0100

    chore: typecheck on dev mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 import { defineNuxtConfig } from 'nuxt/config'
 import vuetify from 'vite-plugin-vuetify'
+import checker from 'vite-plugin-checker'
 
 import { vidos } from './lib/config'
 import { configuredApi, configuredImageProxy } from './utils/vido-config-static'
@@ -105,5 +106,6 @@ export default defineNuxtConfig({
   },
   vite: {
     optimizeDeps: { exclude: ['fsevents'] },
+    plugins: [!process.env.CYPRESS ? checker({ vueTsc: true }) : undefined],
   },
 })
