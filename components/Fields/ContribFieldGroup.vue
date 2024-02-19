35597ff22cd6fbe52e42a4d0e0bd133c97df2753
Noé Viricel <noe.viricel@wazo-lab.io>
2024-02-19 10:33:32 +0100

    chore: use lodash pickBy instead of flow #142

--- components/Fields/ContribFieldGroup.vue
+++ components/Fields/ContribFieldGroup.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 /* eslint-disable vue/prop-name-casing */
-import { flow } from 'lodash'
+import { pickBy } from 'lodash'
 import type { ContribFields, Link } from '~/middleware/contrib-mode.global'
 import ExternalLink from '~/components/UI/ExternalLink.vue'
 
@@ -11,12 +11,8 @@ const props = defineProps<{
 }>()
 
 const onlyDefinedProps = computed(() => {
-  return flow([
-    Object.entries,
-    arr => arr.filter(([_key, value]: [key: string, value: Link ]) => !!value),
-    Object.fromEntries,
-  ])(props)
-})
+  return pickBy(props, p => !!p)
+}) as ComputedRef<Record<string, Link>>
 </script>
 
 <template>
