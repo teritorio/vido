84d0091de25a9eb732c13349c6b5316fafe7c85f
Noé Viricel <38253764+wazolab@users.noreply.github.com>
2024-02-22 10:48:50 +0100

    fix: always one star on favorite notebook (#160)

--- components/Fields/Stars.vue
+++ components/Fields/Stars.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="stars-data tw-text-amber-500 tw-mb-1">
     <FontAwesomeIcon
-      v-for="index in new Array(stars)"
+      v-for="(_item, index) in numericProp"
       :key="index"
       icon="star"
     />
+    {{ isSNotation ? 'S' : '' }}
   </div>
 </template>
 
 <script lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import type { PropType } from 'vue'
 
 import { defineNuxtComponent } from '#app'
+
+enum StarsEnum {
+  One = '1',
+  OneS = '1S',
+  Two = '2',
+  TwoS = '2S',
+  Three = '3',
+  ThreeS = '3S',
+  Four = '4',
+  FourS = '4S',
+  Five = '5',
+  FiveS = '5S',
+}
 
 export default defineNuxtComponent({
   components: {
@@ -20,8 +35,19 @@ export default defineNuxtComponent({
 
   props: {
     stars: {
-      type: Number,
+      type: String as PropType<StarsEnum>,
       required: true,
+    },
+  },
+  computed: {
+    isSNotation(): boolean {
+      return Boolean(this.stars.includes('S'))
+    },
+    numericProp(): number {
+      let stars: string = this.stars
+      if (this.isSNotation) stars = this.stars.slice(0, -1)
+
+      return Number(stars)
     },
   },
 })
