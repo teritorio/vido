578a892396f0640cac8c0d8a808169e9d51aeee9
Noé Viricel <noe.viricel@wazo-lab.io>
2024-02-28 15:39:04 +0100

    chore: update PoisTable to setup rendering #172

--- components/PoisList/PoisTable.vue
+++ components/PoisList/PoisTable.vue
@@ -1,111 +1,97 @@
-<script lang="ts">
-import Field from '~/components/Fields/Field.vue'
-import type { ApiPoi, ApiPoiProperties, ApiPois, FieldsListItem } from '~/lib/apiPois'
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { PropertyTranslationsContextEnum } from '~/plugins/property-translations'
+import type { ApiPois, FieldsListItem } from '~/lib/apiPois'
+import Field from '~/components/Fields/Field.vue'
+import IconButton from '~/components/UI/IconButton.vue'
 import ContribFieldGroup from '~/components/Fields/ContribFieldGroup.vue'
-import type { ContribFields } from '~/composables/useContrib'
 
-interface DataTableHeaders {
+interface DataTableHeader {
+  key: string
   title: string
-  value?: string
-  key?: string
+  value?: string | Function
 }
 
-export default {
-  components: {
-    ContribFieldGroup,
-    Field,
-  },
-  props: {
-    fields: {
-      type: Array as PropType<FieldsListItem[]>,
-      required: true,
-    },
-    pois: {
-      type: Object as PropType<ApiPois>,
-      required: true,
-    },
-  },
+const props = defineProps<{
+  fields: FieldsListItem[]
+  pois: ApiPois
+}>()
 
-  data(): {
-    contribMode: boolean
-    context: string
-    isContribEligible: (properties: ApiPoiProperties) => boolean
-    getContributorFields: (feature: ApiPoi) => ContribFields
-  } {
-    const { contribMode, isContribEligible, getContributorFields } = useContrib()
+const { t } = useI18n()
+const { $propertyTranslations } = useNuxtApp()
+const { contribMode, isContribEligible, getContributorFields } = useContrib()
 
-    return {
-      contribMode,
-      context: PropertyTranslationsContextEnum.List,
-      isContribEligible,
-      getContributorFields,
-    }
-  },
+const headers = computed((): Array<DataTableHeader> => {
+  // Basic Fields
+  const headers: Array<DataTableHeader> = props.fields.map(f => ({
+    key: f.field,
+    title: $propertyTranslations.p(
+      f.field,
+      PropertyTranslationsContextEnum.List,
+    ),
+  }))
 
-  computed: {
-    headers(): Array<DataTableHeaders> {
-      const headers: Array<DataTableHeaders> = this.fields.map(f => ({
-        title: this.$propertyTranslations.p(
-          f.field,
-          PropertyTranslationsContextEnum.List,
-        ),
-        value: f.field,
-        key: `properties.${f.field}`,
-      }))
+  // Contrib Field
+  if (contribMode) {
+    headers.push({
+      key: 'contrib',
+      title: t('fields.contrib.heading'),
+    })
+  }
 
-      headers.push({ title: '' })
+  // Details Field
+  headers.push({
+    key: 'details',
+    title: 'Actions',
+  })
 
-      if (this.contribMode)
-        headers.push({ title: this.$t('fields.contrib.heading') })
-
-      return headers
-    },
-  },
-}
+  return headers
+})
 </script>
 
 <template>
-  <v-data-table
-    class="mt-4"
+  <VDataTable
     :headers="headers"
     :items="pois.features"
     :no-data-text="$t('poisTable.empty')"
-    item-key="name"
-    items-per-page="5"
+    items-per-page="20"
   >
-    <template #item="{ item }">
+    <template #item="{ item, columns }">
       <tr>
-        <td v-for="field in fields" :key="field.field" class="tw-align-top">
-          <Field
-            :context="context"
-            :recursion-stack="[field.field]"
-            :field="field"
-            :details="$t('poisTable.details')"
-            :properties="item.selectable.properties"
-            :geom="item.selectable.geometry"
+        <td v-for="col in columns" :key="col.key">
+          <ContribFieldGroup
+            v-if="col.key === 'contrib' && isContribEligible(item.raw.properties)"
+            v-bind="getContributorFields(item.raw)"
           />
-        </td>
-        <td>
-          <NuxtLink :to="`/poi/${item.selectable.properties.metadata.id}/details`">
-            {{ $t('poisTable.details') }}
-          </NuxtLink>
-        </td>
-        <td v-if="contribMode && isContribEligible(feature.properties)" class="tw-align-top">
-          <ContribFieldGroup v-bind="getContributorFields(feature)" />
+          <IconButton
+            v-else-if="col.key === 'details'"
+            class="tw-h-10"
+            :href="`/poi/${item.raw.properties.metadata.id}/details`"
+            :label="t('poisTable.details')"
+            target="_self"
+          >
+            <FontAwesomeIcon icon="external-link-alt" />
+            {{ t('poisTable.details') }}
+          </IconButton>
+          <Field
+            v-else
+            :context="PropertyTranslationsContextEnum.List"
+            :recursion-stack="[col.key]"
+            :field="{ field: col.key }"
+            :details="t('poisTable.details')"
+            :properties="item.raw.properties"
+            :geom="item.raw.geometry"
+          />
         </td>
       </tr>
     </template>
-  </v-data-table>
+  </VDataTable>
 </template>
 
-<style scoped>
+<style>
 /* stylelint-disable selector-class-pattern */
-.v-data-table .v-table__wrapper > table thead {
-  background: blue
-}
-
-.v-data-table .v-table__wrapper > table tbody > tr:nth-child(even) > td {
+.v-data-table .v-table__wrapper > table tbody > tr:nth-child(even) > td,
+.v-data-table .v-table__wrapper > table > thead > tr th {
   background: #F3F4F6;
 }
 </style>
