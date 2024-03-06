c415c866a19f8757061c772559ef45810874f0c3
Noé Viricel <noe.viricel@wazo-lab.io>
2024-03-06 10:25:43 +0100

    feat: add PropertyTranslations support + create plugin's Vue version #137

--- plugins/property-translations.ts
+++ plugins/property-translations.ts
@@ -1,3 +1,4 @@
+import type { App } from 'vue'
 import { defineNuxtPlugin } from '#app/nuxt'
 import type { PropertyTranslations } from '~/lib/apiPropertyTranslations'
 
@@ -11,46 +12,52 @@ export enum PropertyTranslationsContextEnum {
 const Default = PropertyTranslationsContextEnum.Default
 
 export interface PropertyTranslationsPlugin {
-  set(propertyTranslations: PropertyTranslations): void
-  p(propertyName: string, context: PropertyTranslationsContextEnum): string
-  pv(
+  set: (propertyTranslations: PropertyTranslations) => void
+  p: (propertyName: string, context: PropertyTranslationsContextEnum) => string
+  pv: (
     propertyName: string,
     valueName: string,
     context: PropertyTranslationsContextEnum
-  ): string
+  ) => string
 
   propertyTranslations: PropertyTranslations
 }
 
-export default defineNuxtPlugin((_nuxtApp) => {
-  const pt: PropertyTranslationsPlugin = {
-    propertyTranslations: {},
-    set(setPropertyTranslations: PropertyTranslations): void {
-      pt.propertyTranslations = setPropertyTranslations
-    },
-    p(
-      propertyName: string,
-      context: PropertyTranslationsContextEnum = Default,
-    ): string {
-      const pn = pt.propertyTranslations[propertyName]
-      return (
-        // When context exists, does not use default
-        (pn?.[context] ? pn?.[context]?.fr : pn?.[Default]?.fr) || propertyName
-      )
-    },
-    pv(
-      propertyName: string,
-      valueName: string,
-      context: PropertyTranslationsContextEnum = Default,
-    ): string {
-      const pn = pt.propertyTranslations[propertyName]?.values?.[valueName]
-      return (
-        // When context exists, does not use default
-        (pn?.[context] ? pn?.[context]?.fr : pn?.[Default]?.fr) || valueName
-      )
-    },
-  }
+const pt: PropertyTranslationsPlugin = {
+  propertyTranslations: {},
+  set(setPropertyTranslations: PropertyTranslations): void {
+    pt.propertyTranslations = setPropertyTranslations
+  },
+  p(
+    propertyName: string,
+    context: PropertyTranslationsContextEnum = Default,
+  ): string {
+    const pn = pt.propertyTranslations[propertyName]
+    return (
+      // When context exists, does not use default
+      (pn?.[context] ? pn?.[context]?.fr : pn?.[Default]?.fr) || propertyName
+    )
+  },
+  pv(
+    propertyName: string,
+    valueName: string,
+    context: PropertyTranslationsContextEnum = Default,
+  ): string {
+    const pn = pt.propertyTranslations[propertyName]?.values?.[valueName]
+    return (
+      // When context exists, does not use default
+      (pn?.[context] ? pn?.[context]?.fr : pn?.[Default]?.fr) || valueName
+    )
+  },
+}
 
+export const propertyTranslationsVuePlugin = {
+  install(app: App, _options?: object) {
+    app.config.globalProperties.$propertyTranslations = pt
+  },
+}
+
+export default defineNuxtPlugin((_nuxtApp) => {
   return {
     provide: {
       propertyTranslations: pt,
