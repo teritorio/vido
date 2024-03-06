2a819118a520f0136d110d1671300c2ff049cc7d
Noé Viricel <noe.viricel@wazo-lab.io>
2024-03-06 13:02:52 +0100

    feat: add vue auto-import global components support #137

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import { resolve } from 'node:path'
+import { mergeConfig } from 'vite'
 import type { StorybookConfig } from '@storybook/vue3-vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 const config: StorybookConfig = {
   stories: [
@@ -18,21 +20,28 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   viteFinal: async (config) => {
-    (config.define = { 'process.env': {} })
-    if (config.resolve) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '~': resolve(__dirname, '../'),
-        // Maybe we could take advantage of auto-imports
-        '#app': resolve(__dirname, '../node_modules/nuxt/dist/app'),
-        '#head': resolve(__dirname, '../node_modules/nuxt/dist/head/runtime'),
-        '#build': resolve(__dirname, '../.nuxt'),
-        '#imports': resolve(__dirname, '../.nuxt/imports'),
-        '#image': resolve(__dirname, '../node_modules/@nuxt/image-edge/dist/runtime'),
-      }
-    }
-
-    return config
+    return mergeConfig(config, {
+      define: { 'process.env': {} },
+      resolve: {
+        alias: {
+          '~': resolve(__dirname, '../'),
+          // Maybe we could take advantage of auto-imports
+          '#app': resolve(__dirname, '../node_modules/nuxt/dist/app'),
+          '#head': resolve(__dirname, '../node_modules/nuxt/dist/head/runtime'),
+          '#build': resolve(__dirname, '../.nuxt'),
+          '#imports': resolve(__dirname, '../.nuxt/imports'),
+          '#image': resolve(__dirname, '../node_modules/@nuxt/image-edge/dist/runtime'),
+        },
+      },
+      plugins: [
+        AutoImport({
+          dts: './.storybook/auto-imports.d.ts',
+          imports: [
+            'vue',
+          ],
+        }),
+      ],
+    })
   },
 }
 export default config
