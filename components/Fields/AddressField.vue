5a00067f0377a971d2795c6818a7fbcd8293a744
Noé Viricel <noe.viricel@wazo-lab.io>
2024-03-13 10:39:14 +0100

    refactor: use useField composable in AddressField component

--- components/Fields/AddressField.vue
+++ components/Fields/AddressField.vue
@@ -4,22 +4,6 @@ import type { PropType } from 'vue'
 import { defineNuxtComponent } from '#app'
 import type { ApiPoiProperties } from '~/lib/apiPois'
 
-const addressFields = [
-  'addr:housenumber',
-  'addr:street',
-  'addr:postcode',
-  'addr:city',
-]
-
-export function isAddressFieldEmpty(properties: {
-  [key: string]: string
-}): boolean {
-  return addressFields.reduce(
-    (sum: boolean, value) => sum || value in properties,
-    false,
-  )
-}
-
 export default defineNuxtComponent({
   props: {
     properties: {
@@ -29,19 +13,16 @@ export default defineNuxtComponent({
   },
 
   computed: {
-    address(): string | null {
-      return addressFields
-        .map(field => this.properties[field])
-        .map(f => (f || '').toString().trim())
-        .filter(f => f)
-        .join(' ')
+    address() {
+      const { addressToString } = useField()
+      return addressToString(this.properties)
     },
   },
 })
 </script>
 
 <template>
-  <div v-if="address">
+  <div>
     <slot />
     <span>
       {{ address }}
