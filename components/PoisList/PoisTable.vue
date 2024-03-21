44ea01ea6aa7710d7b56a60a57fc43a06c99545e
Noé Viricel <noe.viricel@wazo-lab.io>
2024-03-21 10:40:36 +0100

    feat: open details link in new tab when in embedded context #191

--- components/PoisList/PoisTable.vue
+++ components/PoisList/PoisTable.vue
@@ -22,6 +22,7 @@ interface DataTableHeader {
 
 const props = defineProps<{
   category?: ApiMenuCategory
+  detailsIsExternal?: boolean
 }>()
 
 const device = useDevice()
@@ -221,7 +222,7 @@ function getContext(key: string) {
               class="tw-h-10"
               :href="`/poi/${item.properties.metadata.id}/details`"
               :label="t('poisTable.details')"
-              target="_self"
+              :target="detailsIsExternal ? '_blank' : '_self'"
             >
               <FontAwesomeIcon icon="external-link-alt" />
               {{ t('poisTable.details') }}
