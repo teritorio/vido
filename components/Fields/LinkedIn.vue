b98b5e454a7f3bee5a3e35b4edc3edee4530975d
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-25 16:30:23 +0200

    feat: add linkedin support to field component #268

---
+++ components/Fields/LinkedIn.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import type { PropType } from 'vue'
+
+import { defineNuxtComponent } from '#app'
+
+export default defineNuxtComponent({
+  components: {
+    FontAwesomeIcon,
+  },
+
+  props: {
+    url: {
+      type: String as PropType<string>,
+      default: null,
+    },
+  },
+})
+</script>
+
+<template>
+  <a v-if="url" :href="url" target="_blank">
+    <FontAwesomeIcon icon="fa-brands fa-linkedin" size="lg" />
+  </a>
+</template>
