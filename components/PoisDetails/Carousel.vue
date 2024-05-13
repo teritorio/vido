b944f47d0ae55ccf90c30183e8a779d33495bea6
Noé Viricel <noe.viricel@wazo-lab.io>
2024-05-13 15:48:20 +0200

    fix: carousel image ratio #127

--- components/PoisDetails/Carousel.vue
+++ components/PoisDetails/Carousel.vue
@@ -43,14 +43,13 @@ export default defineNuxtComponent({
       :show-arrows="false"
       :hide-delimiter-background="true"
       class="tw-mb-14 print:tw-mb-6"
-      :height="device.smallScreen ? 300 : 500"
+      height="auto"
     >
       <v-carousel-item v-for="(image, i) in images" :key="i">
         <UIPicture
           :src="image"
           :media-size="device.smallScreen ? '100vw' : '66vw'"
           :alt="$t('poiCard.image')"
-          :img-attrs="{ class: 'h-100 tw-object-cover' }"
         />
       </v-carousel-item>
     </v-carousel>
@@ -62,5 +61,12 @@ export default defineNuxtComponent({
   color: #000;
   border: 2px solid #fff;
   border-radius: 100%;
+}
+
+:deep(picture img) {
+  aspect-ratio: 16/9;
+  object-fit: cover;
+  height: auto;
+  width: 100%;
 }
 </style>
