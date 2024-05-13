ba99054715faef3feaa0b170c790bab054c61627
Noé Viricel <noe.viricel@wazo-lab.io>
2024-05-13 16:13:53 +0200

    chore: add OSM link to details footer #280

--- components/PoisDetails/PoiDetails.vue
+++ components/PoisDetails/PoiDetails.vue
@@ -305,7 +305,14 @@ export default defineNuxtComponent({
     <template #footer>
       <span v-if="poi.properties.metadata.updated_at">
         {{ $t('poiDetails.lastUpdate') }}
-        <RelativeDate :date="poi.properties.metadata.updated_at" />
+        <a
+          v-if="poi.properties.metadata.osm_type && poi.properties.metadata.osm_id"
+          :href="`https://www.openstreetmap.org/${poi.properties.metadata.osm_type}/${poi.properties.metadata.osm_id}`"
+          target="_blank"
+        >
+          <RelativeDate :date="poi.properties.metadata.updated_at" />
+        </a>
+        <RelativeDate v-else :date="poi.properties.metadata.updated_at" />
       </span>
     </template>
   </PoiLayout>
