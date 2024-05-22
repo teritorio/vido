d76bc864207f53f4621acd70b0001c25bbe6d2eb
Noé Viricel <noe.viricel@wazo-lab.io>
2024-05-22 17:05:28 +0200

    chore: upgrade nuxt v3.4.0 #290

--- plugins/sentry.ts
+++ plugins/sentry.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const router = useRouter()
-  const { public: { sentry } } = useRuntimeConfig()
+  const { sentry } = useRuntimeConfig().public
 
   if (!sentry.dsn)
     return
