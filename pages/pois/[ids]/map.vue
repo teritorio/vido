9fce99de8981884f8999c8d57f7dbe3ced0c0a3e
Noé Viricel <noe.viricel@wazo-lab.io>
2024-05-22 15:16:37 +0200

    chore: apply script setup style to Pois map page #290

--- pages/pois/[ids]/map.vue
+++ pages/pois/[ids]/map.vue
@@ -1,111 +1,84 @@
-<script lang="ts">
-import { mapWritableState } from 'pinia'
-import type { Ref } from 'vue'
-import { ref } from 'vue'
-
-import { defineNuxtComponent, useRequestHeaders, useRoute } from '#app'
-import { definePageMeta } from '#imports'
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import MapPois from '~/components/Map/MapPois.vue'
-import type { ApiPoiId, ApiPois } from '~/lib/apiPois'
-import { getPois } from '~/lib/apiPois'
-import type { Settings } from '~/lib/apiSettings'
-import { getSettings, headerFromSettings } from '~/lib/apiSettings'
+import { type ApiPoiId, type ApiPois, getPois } from '~/lib/apiPois'
+import { type Settings, getSettings, headerFromSettings } from '~/lib/apiSettings'
 import { getAsyncDataOrThrows } from '~/lib/getAsyncData'
 import { vidoConfig } from '~/plugins/vido-config'
 import { siteStore } from '~/stores/site'
 import type { VidoConfig } from '~/utils/types-config'
 
-export default defineNuxtComponent({
-  components: {
-    MapPois,
-  },
-
-  async setup(): Promise<{
-    config: VidoConfig
-    settings: Ref<Settings>
-    pois: Ref<ApiPois | null>
-  }> {
-    definePageMeta({
-      validate({ params }) {
-        return (
-          typeof params.ids === 'string'
-          && /^[,-_:a-zA-Z0-9]+$/.test(params.ids)
-        )
-      },
-    })
-
-    const params = useRoute().params
-    const configRef = await getAsyncDataOrThrows('configRef', () =>
-      Promise.resolve(siteStore().config || vidoConfig(useRequestHeaders())))
-    const config: VidoConfig = configRef.value
-
-    const fetchSettings = getAsyncDataOrThrows('fetchSettings', () =>
-      siteStore().settings
-        ? Promise.resolve(siteStore().settings as Settings)
-        : getSettings(config))
-
-    let settings: Ref<Settings>
-    let pois: Ref<ApiPois | null>
-    if (params.ids) {
-      const ids = (params.ids as string).split(',')
-      const getPoiPromise = getAsyncDataOrThrows('getPoiPromise', () =>
-        getPois(config, ids, {
-          geometry_as: undefined,
-        }))
-      const [settingsF, poisF] = await Promise.all([
-        fetchSettings,
-        getPoiPromise,
-      ])
-
-      settings = settingsF
-      pois = poisF
-    }
-    else {
-      const [settingsF] = await Promise.all([fetchSettings])
-
-      settings = settingsF
-      pois = ref(null)
-    }
-    useHead(headerFromSettings(settings.value))
-
-    return {
-      config,
-      settings,
-      pois,
-    }
-  },
-
-  computed: {
-    ...mapWritableState(siteStore, {
-      locale: 'locale',
-      globalConfig: 'config',
-      globalSettings: 'settings',
-    }),
-
-    ids(): ApiPoiId[] {
-      return (
-        this.pois?.features.map(feature => feature.properties.metadata.id)
-          || []
-      )
-    },
-  },
-
-  created() {
-    this.globalConfig = this.config
-    this.globalSettings = this.settings
-
-    this.$settings.set(this.settings)
-  },
-
-  beforeMount() {
-    this.$trackingInit(this.config)
-    this.$vidoConfigSet(this.config)
-  },
-
-  mounted() {
-    this.locale = this.$i18n.locale
+definePageMeta({
+  validate({ params }) {
+    return (
+      typeof params.ids === 'string'
+      && /^[,-_:a-zA-Z0-9]+$/.test(params.ids)
+    )
   },
 })
+
+const params = useRoute().params
+const configRef = await getAsyncDataOrThrows('configRef', () =>
+  Promise.resolve(siteStore().config || vidoConfig(useRequestHeaders())))
+const config: VidoConfig = configRef.value
+
+const fetchSettings = getAsyncDataOrThrows('fetchSettings', () =>
+  siteStore().settings
+    ? Promise.resolve(siteStore().settings as Settings)
+    : getSettings(config))
+
+let settings: Ref<Settings>
+let pois: Ref<ApiPois | null>
+
+if (params.ids) {
+  const ids = (params.ids as string).split(',')
+  const getPoiPromise = getAsyncDataOrThrows('getPoiPromise', async () =>
+    await getPois(config, ids, {
+      geometry_as: undefined,
+    }))
+  const [settingsF, poisF] = await Promise.all([
+    fetchSettings,
+    getPoiPromise,
+  ])
+
+  settings = settingsF
+  pois = poisF
+}
+else {
+  const [settingsF] = await Promise.all([fetchSettings])
+
+  settings = settingsF
+  pois = ref(null)
+}
+
+const ids = computed((): ApiPoiId[] => {
+  return pois.value?.features.map(feature => feature.properties.metadata.id) || []
+})
+
+const { $trackingInit, $vidoConfigSet } = useNuxtApp()
+onBeforeMount(() => {
+  $trackingInit(config)
+  $vidoConfigSet(config)
+})
+
+const {
+  locale,
+  config: globalConfig,
+  settings: globalSettings,
+} = storeToRefs(siteStore())
+
+const { locale: i18nLocale } = useI18n()
+onMounted(() => {
+  locale.value = i18nLocale.value
+})
+
+globalConfig.value = config
+globalSettings.value = settings.value
+
+const { $settings } = useNuxtApp()
+$settings.set(settings.value)
+
+useHead(headerFromSettings(settings.value))
 </script>
 
 <template>
