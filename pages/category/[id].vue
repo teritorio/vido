4817e63f9fb93403edb9dd790f29f048dd5a5c76
Noé Viricel <noe.viricel@wazo-lab.io>
2024-06-20 01:08:28 +0200

    refactor: merge common logic into category index page #319

--- pages/category/[id].vue
+++ pages/category/[id].vue
@@ -1,12 +1,4 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { menuStore as useMenuStore } from '~/stores/menu'
-import { siteStore as useSiteStore } from '~/stores/site'
-import Header from '~/components/Layout/Header.vue'
-import Footer from '~/components/Layout/Footer.vue'
-import PoisTable from '~/components/PoisList/PoisTable.vue'
-import CategorySelector from '~/components/PoisList/CategorySelector.vue'
-
 //
 // Validators
 //
@@ -17,71 +9,4 @@ definePageMeta({
     )
   },
 })
-
-//
-// Composables
-//
-const siteStore = useSiteStore()
-const { config, settings, contents } = storeToRefs(siteStore)
-const menuStore = useMenuStore()
-const { menuItems } = storeToRefs(menuStore)
-const { params } = useRoute()
-const router = useRouter()
-const id = Number.parseInt(params.id as string)
-const { $trackingInit } = useNuxtApp()
-
-//
-// Hooks
-//
-onBeforeMount(() => {
-  $trackingInit(config.value!)
-})
-
-//
-// Methods
-//
-function onCategoryUpdate(categoryId: number) {
-  if (!categoryId)
-    return
-
-  router.push(`/category/${categoryId}`)
-}
 </script>
-
-<template>
-  <VContainer fluid>
-    <Header
-      class="mb-4"
-      :theme="settings!.themes[0]"
-      :nav-menu-entries="contents!"
-    >
-      <template #search>
-        <CategorySelector
-          class="w-50"
-          :menu-items="menuItems || {}"
-          :category-id="id"
-          @category-change="onCategoryUpdate"
-        />
-      </template>
-    </Header>
-    <PoisTable />
-    <Footer :attributions="settings!.attributions" />
-  </VContainer>
-</template>
-
-<style lang="scss" scoped>
-@import '~/assets/details';
-
-.page-index {
-  color: $color-text;
-  background-color: #fefefe;
-  padding: 1rem;
-  min-width: 21rem;
-  -webkit-font-smoothing: antialiased;
-  text-rendering: optimizelegibility;
-  line-height: 1.3;
-  word-wrap: break-word;
-
-  @extend %font-light;
-}
-</style>
