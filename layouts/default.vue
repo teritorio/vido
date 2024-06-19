7b48e48323e5ce817f0aaab8e9ace448b218dfe4
Noé Viricel <noe.viricel@wazo-lab.io>
2024-06-20 00:15:29 +0200

    fix: apply translation to propertyTranslations plugin #316

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,6 +13,7 @@ const siteStore = useSiteStore()
 const { config, settings, contents, translations, locale } = storeToRefs(siteStore)
 const menuStore = useMenuStore()
 const { menuItems } = storeToRefs(menuStore)
+const { $propertyTranslations } = useNuxtApp()
 
 if (process.server) {
   await siteStore.init(useRequestHeaders())
@@ -35,6 +36,7 @@ if (!contents.value)
 if (!translations.value)
   throw createError({ statusCode: 500, statusMessage: 'Failed to fetch translations', fatal: true })
 
+$propertyTranslations.set(translations.value)
 useHead(headerFromSettings(settings.value, { googleSiteVerification: config.value!.GOOGLE_SITE_VERIFICATION }))
 locale.value = i18nLocale.value
 </script>
