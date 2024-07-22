9ed03c85284c3f03ff8be97ece8d8e2a811c29be
Noé Viricel <noe.viricel@wazo-lab.io>
2024-07-22 15:55:27 +0200

    refactor: poi-card to script setup style #338

--- components/PoisCard/PoiCard.vue
+++ components/PoisCard/PoiCard.vue
@@ -1,72 +1,44 @@
-<script lang="ts">
-import type { PropType } from 'vue'
-
-import { defineNuxtComponent } from '#app'
+<script setup lang="ts">
 import PoiCardContent from '~/components/PoisCard/PoiCardContent.vue'
 import TeritorioIcon from '~/components/UI/TeritorioIcon.vue'
 import UIButton from '~/components/UI/UIButton.vue'
 import UIPicture from '~/components/UI/UIPicture.vue'
-import type { ApiPoi, ApiPoiId } from '~/lib/apiPois'
+import type { ApiPoi } from '~/lib/apiPois'
 import useDevice from '~/composables/useDevice'
 
-export default defineNuxtComponent({
-  components: {
-    PoiCardContent,
-    TeritorioIcon,
-    UIButton,
-    UIPicture,
-  },
-
-  props: {
-    canClose: {
-      type: Boolean,
-      default: true,
-    },
-    poi: {
-      type: Object as PropType<ApiPoi>,
-      required: true,
-    },
-    explorerModeEnabled: {
-      type: Boolean,
-      required: true,
-    },
-    favoritesModeEnabled: {
-      type: Boolean,
-      required: true,
-    },
-    showImage: {
-      type: Boolean,
-      default: true,
-    },
-  },
-
-  setup() {
-    const device = useDevice()
-
-    return {
-      device,
-    }
-  },
-
-  computed: {
-    id(): ApiPoiId {
-      return this.poi.properties.metadata.id
-    },
-
-    colorFill(): string {
-      return this.poi.properties.display?.color_fill || 'black'
-    },
-
-    icon(): string | undefined {
-      return this.poi.properties.display?.icon
-    },
-  },
+//
+// Props
+//
+withDefaults(defineProps<{
+  canClose: boolean
+  poi: ApiPoi
+  explorerModeEnabled: boolean
+  favoritesModeEnabled: boolean
+  showImage?: boolean
+}>(), {
+  canClose: false,
+  showImage: true,
 })
+
+//
+// Events
+//
+defineEmits<{
+  (e: 'exploreClick', poi: ApiPoi): void
+  (e: 'favoriteClick', poi: ApiPoi): void
+  (e: 'onClose'): void
+  (e: 'zoomClick', poi: ApiPoi): void
+}>()
+
+//
+// Composables
+//
+const device = useDevice()
 </script>
 
 <template>
   <div
-    :id="`PoiCard-${id}`"
+    :id="`PoiCard-${poi.properties.metadata.id}`"
     class="poiDescription tw-z-10 tw-flex tw-flex-col tw-w-full md:tw-max-w-xl tw-mx-0 tw-overflow-y-auto tw-shadow-md tw-pointer-events-auto md:tw-flex-row md:tw-w-auto md:tw-mx-auto md:tw-rounded-xl tw-bg-white"
   >
     <UIButton
@@ -83,14 +55,14 @@ export default defineNuxtComponent({
       class=" tw-bg-gray-100 tw-text-gray-100 tw-flex tw-relative tw-items-center tw-align-middle tw-justify-center tw-h-44 md:tw-w-48 md:tw-h-auto md:tw-max-h-full min-icon-height"
     >
       <TeritorioIcon
-        v-if="icon"
-        :picto="icon"
+        v-if="poi.properties.display?.icon"
+        :picto="poi.properties.display.icon"
         :use-native-alignment="false"
         class="tw-text-8xl tw-align-middle tw-absolute tw-z-0"
         :color-text="
           poi.properties.image && poi.properties.image.length > 0
             ? '#AAA'
-            : colorFill
+            : poi.properties.display?.color_fill || 'black'
         "
       />
       <UIPicture
@@ -107,7 +79,9 @@ export default defineNuxtComponent({
       :explorer-mode-enabled="explorerModeEnabled"
       :favorites-mode-enabled="favoritesModeEnabled"
       class="tw-px-4 tw-py-5 tw-flex tw-flex-col md:tw-overflow-y-auto tw-flex-grow md:tw-max-h-full tw-box-border tw-w-full md:tw-h-80 md:tw-w-96"
-      v-bind="$attrs"
+      @explore-click="$emit('exploreClick', $event)"
+      @favorite-click="$emit('favoriteClick', $event)"
+      @zoom-click="$emit('zoomClick', $event)"
     />
   </div>
 </template>
