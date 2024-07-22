0d90d9b14017d4b7da40b6fd18a9d28667f0e942
Noé Viricel <noe.viricel@wazo-lab.io>
2024-07-22 17:51:09 +0200

    refactor: isolate poi-card from poi-card-light #338

--- components/PoisCard/PoisDeck.vue
+++ components/PoisCard/PoisDeck.vue
@@ -3,12 +3,15 @@ import type { ApiPoi } from '~/lib/apiPois'
 import PoiCard from '~/components/PoisCard/PoiCard.vue'
 import PoiCardLight from '~/components/PoisCard/PoiCardLight.vue'
 
-defineProps<{
-  explorerModeEnabled: boolean
-  favoritesModeEnabled: boolean
+withDefaults(defineProps<{
+  explorerModeEnabled?: boolean
+  favoritesModeEnabled?: boolean
   pois: ApiPoi[]
   isCardLight: boolean
-}>()
+}>(), {
+  explorerModeEnabled: false,
+  favoritesModeEnabled: false,
+})
 
 defineEmits<{
   (e: 'exploreClick', poi: ApiPoi): void
@@ -18,9 +21,18 @@ defineEmits<{
 </script>
 
 <template>
-  <div class="tw-flex tw-justify-between tw-flex-wrap tw-gap-6">
+  <div v-if="isCardLight" class="tw-flex tw-justify-between tw-flex-wrap tw-gap-6">
     <component
-      :is="isCardLight ? PoiCardLight : PoiCard"
+      :is="PoiCardLight"
+      v-for="item in pois"
+      :key="item.properties.metadata.id"
+      :poi="item"
+      class="tw-grow-1 poi-deck"
+    />
+  </div>
+  <div v-else class="tw-flex tw-justify-between tw-flex-wrap tw-gap-6">
+    <component
+      :is="PoiCard"
       v-for="item in pois"
       :key="item.properties.metadata.id"
       :can-close="false"
