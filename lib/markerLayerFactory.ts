d23548512b4d8b89e552badf2dd70e0056510132
Noé Viricel <noe.viricel@wazo-lab.io>
2024-07-30 17:17:08 +0200

    chore: update maplibre to v4.0.0 #343

--- lib/markerLayerFactory.ts
+++ lib/markerLayerFactory.ts
@@ -1,5 +1,6 @@
 import type {
   FitBoundsOptions,
+  GeoJSONSource,
   LayerSpecification,
   LngLatBounds,
   Map,
@@ -112,105 +113,96 @@ export function updateMarkers(
   const markerIdPrevious = Object.keys(markers)
   const markerIdcurrent: string[] = []
 
-  const features = map.querySourceFeatures(src)
+  const features = map.querySourceFeatures(src).filter(feature => feature.geometry.type === 'Point')
   // for every cluster on the screen, create an HTML marker for it (if we didn't yet),
   // and add it to the map if it's not there already
-  features
-    .filter(feature => feature.geometry.type === 'Point')
-    .forEach((feature) => {
-      const coords = (feature.geometry as GeoJSON.Point).coordinates as [
-        number,
-        number,
-      ]
-      const props = feature.properties
-      if (props?.cluster) {
-        const id = `c${props.cluster_id}`
-        markerIdcurrent.push(id)
-        if (!markers[id]) {
-          const {
-            cluster: _a,
-            cluster_id: _b,
-            point_count,
-            _c: _d,
-            point_count_abbreviated: _e,
-            ...countPercolor
-          } = props
-          const element = createMarkerDonutChart(countPercolor, point_count)
+  for (let i = 0; i < features.length; i++) {
+    const coords = (features[i].geometry as GeoJSON.Point).coordinates as [
+      number,
+      number,
+    ]
+    const props = features[i].properties
+    if (props?.cluster) {
+      const id = `c${props.cluster_id}`
+      markerIdcurrent.push(id)
+      if (!markers[id]) {
+        const {
+          cluster: _a,
+          cluster_id: _b,
+          point_count,
+          _c: _d,
+          point_count_abbreviated: _e,
+          ...countPercolor
+        } = props
+        const element = createMarkerDonutChart(countPercolor, point_count)
 
-          markers[id] = createMarker(coords, { element })
-          markers[id].addTo(map)
+        markers[id] = createMarker(coords, { element })
+        markers[id].addTo(map)
 
-          element.addEventListener('click', (e) => {
-            e.stopPropagation()
-            const source = map.getSource(src)
+        element.addEventListener('click', async (e) => {
+          e.stopPropagation()
+          const source = map.getSource(src) as GeoJSONSource
 
-            if (source && 'getClusterLeaves' in source) {
-              // @ts-expect-error: getClusterLeaves is for GeoJSONSource but source is Source type
-              source.getClusterLeaves(
-                props.cluster_id,
-                100,
-                0,
-                (error: any, features: GeoJSON.Feature[]) => {
-                  if (!error && map) {
-                    const bounds = getBBoxFeatures(features)
-                    if (bounds)
-                      fitBounds(bounds, {})
-                  }
-                },
-              )
+          if (source && 'getClusterLeaves' in source) {
+            const leaves = await source.getClusterLeaves(props.cluster_id, props.point_count, 0)
+            if (leaves.length) {
+              const bounds = getBBoxFeatures(leaves)
+              if (bounds)
+                fitBounds(bounds, {})
             }
-          })
-        }
-      }
-      else if (props?.metadata) {
-        if (typeof props.metadata === 'string')
-          props.metadata = JSON.parse(props.metadata)
-
-        const id = `m${feature.id}`
-        markerIdcurrent.push(id)
-
-        // Workaround to correct shifting POI markers after zoom-in
-        if (!markers[id] || (markers[id] && (markers[id]?.getLngLat().lat !== (feature.geometry as GeoJSON.Point).coordinates[1]))) {
-          if (markers[id])
-            markers[id].remove()
-
-          const markerCoords
-            = feature.geometry.type === 'Point'
-            && (feature.geometry.coordinates as TupleLatLng)
-          if (markerCoords) {
-            if (typeof props.display === 'string')
-              props.display = JSON.parse(props.display)
-
-            if (typeof props.editorial === 'string')
-              props.editorial = JSON.parse(props.editorial)
-
-            // Marker
-            markers[id] = makerHtmlFactory(
-              id,
-              markerCoords, // Using this to avoid misplaced marker
-              props.display?.color_fill || '#000000',
-              props.display?.icon || '#000000',
-              props['image:thumbnail'],
-              null,
-              props.display?.text,
-            )
-
-            // Click handler
-            if (markerClickCallBack && props.editorial?.popup_fields) {
-              const el = markers[id].getElement()
-
-              el.addEventListener('click', (e: MouseEvent) => {
-                e.stopPropagation()
-                const pinMarker = createMarker(markerCoords)
-
-                markerClickCallBack(feature as unknown as ApiPoi, pinMarker)
-              })
-            }
-            markers[id].addTo(map)
           }
+        })
+      }
+    }
+    else if (props?.metadata) {
+      if (typeof props.metadata === 'string')
+        props.metadata = JSON.parse(props.metadata)
+
+      const id = `m${features[i].id}`
+      markerIdcurrent.push(id)
+
+      // Workaround to correct shifting POI markers after zoom-in
+      if (!markers[id] || (markers[id] && (markers[id]?.getLngLat().lat !== (features[i].geometry as GeoJSON.Point).coordinates[1]))) {
+        if (markers[id])
+          markers[id].remove()
+
+        const markerCoords
+            = features[i].geometry.type === 'Point'
+            && ((features[i].geometry as GeoJSON.Point).coordinates as TupleLatLng)
+        if (markerCoords) {
+          if (typeof props.display === 'string')
+            props.display = JSON.parse(props.display)
+
+          if (typeof props.editorial === 'string')
+            props.editorial = JSON.parse(props.editorial)
+
+          // Marker
+          markers[id] = makerHtmlFactory(
+            id,
+            markerCoords, // Using this to avoid misplaced marker
+            props.display?.color_fill || '#000000',
+            props.display?.icon || '#000000',
+            props['image:thumbnail'],
+            null,
+            props.display?.text,
+          )
+
+          // Click handler
+          if (markerClickCallBack && props.editorial?.popup_fields) {
+            const el = markers[id].getElement()
+
+            el.addEventListener('click', (e: MouseEvent) => {
+              e.stopPropagation()
+              const pinMarker = createMarker(markerCoords)
+
+              markerClickCallBack(features[i] as unknown as ApiPoi, pinMarker)
+            })
+          }
+          markers[id].addTo(map)
         }
       }
-    })
+    }
+  }
 
   // for every marker we've added previously, remove those that are no longer visible
   const markerIdcurrentSet = new Set(markerIdcurrent)
