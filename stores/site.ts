86fcc13a19fa8c6208849b26f7a7043f33951969
Noé Viricel <noe.viricel@wazo-lab.io>
2025-03-03 23:20:52 +0100

    refactor: site store to composition api #462

--- stores/site.ts
+++ stores/site.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import type { Article } from '~/lib/apiArticle'
 import type { PropertyTranslations } from '~/lib/apiPropertyTranslations'
-import type { Settings } from '~/lib/apiSettings'
+import type { Settings, SiteInfosTheme } from '~/lib/apiSettings'
 import type { VidoConfig } from '~/utils/types-config'
 import { getArticles } from '~/lib/apiArticle'
 import { getPropertyTranslations } from '~/lib/apiPropertyTranslations'
@@ -17,69 +17,71 @@ export enum PropertyTranslationsContextEnum {
 
 const Default = PropertyTranslationsContextEnum.Default
 
-interface State {
-  locale: string | null
-  config: VidoConfig | undefined
-  explorerModeEnabled: boolean
-  favoritesModeEnabled: boolean
-  settings: Settings | undefined
-  articles: Article[]
-  translations: PropertyTranslations | undefined
-}
+export const siteStore = defineStore('site', () => {
+  const locale = ref<string>()
+  const config = ref<VidoConfig>()
+  const settings = ref<Settings>()
+  const articles = ref<Article[]>([])
+  const translations = ref<PropertyTranslations>()
+  const theme = ref<SiteInfosTheme | undefined>(settings.value?.themes[0])
+  const explorerModeEnabled = ref<SiteInfosTheme['explorer_mode']>(theme.value?.explorer_mode ?? true)
+  const favoritesModeEnabled = ref<SiteInfosTheme['favorites_mode']>(theme.value?.favorites_mode ?? true)
 
-export const siteStore = defineStore('site', {
-  state: (): State => ({
-    locale: null,
-    config: undefined,
-    explorerModeEnabled: false,
-    favoritesModeEnabled: false,
-    settings: undefined,
-    articles: [],
-    translations: undefined,
-  }),
-  actions: {
-    async init(headers: Record<string, string>) {
-      this.config = vidoConfig(headers)
-      this.settings = await getSettings(this.config)
-      this.explorerModeEnabled = this.settings?.themes[0]?.explorer_mode || true
-      this.favoritesModeEnabled = this.settings?.themes[0]?.favorites_mode || true
-      this.translations = await getPropertyTranslations(this.config)
-      const { data: articlesData, error: articlesError, status: articlesStatus } = await getArticles(this.config)
+  async function init(headers: Record<string, string>) {
+    config.value = vidoConfig(headers)
+    settings.value = await getSettings(config.value)
+    translations.value = await getPropertyTranslations(config.value)
+    const { data: articlesData, error: articlesError, status: articlesStatus } = await getArticles(config.value)
 
-      if (articlesError.value)
-        throw createError(articlesError.value)
+    if (articlesError.value)
+      throw createError(articlesError.value)
 
-      if (articlesStatus.value === 'success' && articlesData.value) {
-        this.articles = articlesData.value
-      }
-    },
-    p(propertyName: string, context: PropertyTranslationsContextEnum = Default): string {
-      if (!this.translations)
-        throw new Error('Missing translation, call siteStore.init() first.')
+    if (articlesStatus.value === 'success' && articlesData.value) {
+      articles.value = articlesData.value
+    }
+  }
 
-      const pn = this.translations[propertyName]
+  function p(propertyName: string, context: PropertyTranslationsContextEnum = Default): string {
+    if (!translations.value)
+      throw new Error('Missing translation, call siteStore.init() first.')
 
-      // When context exists, does not use default
-      return (
-        pn?.[context]
-          ? pn?.[context]?.fr
-          : pn?.[Default]?.fr
-      )
-      || propertyName
-    },
-    pv(propertyName: string, valueName: string, context: PropertyTranslationsContextEnum = Default): string {
-      if (!this.translations)
-        throw new Error('Missing translation, call siteStore.init() first.')
+    const pn = translations.value[propertyName]
 
-      const pn = this.translations[propertyName]?.values?.[valueName]
+    // When context exists, does not use default
+    return (
+      pn?.[context]
+        ? pn?.[context]?.fr
+        : pn?.[Default]?.fr
+    )
+    || propertyName
+  }
 
-      // When context exists, does not use default
-      return (
-        pn?.[context]
-          ? pn?.[context]?.fr
-          : pn?.[Default]?.fr
-      )
-      || valueName
-    },
-  },
+  function pv(propertyName: string, valueName: string, context: PropertyTranslationsContextEnum = Default): string {
+    if (!translations.value)
+      throw new Error('Missing translation, call siteStore.init() first.')
+
+    const pn = translations.value[propertyName]?.values?.[valueName]
+
+    // When context exists, does not use default
+    return (
+      pn?.[context]
+        ? pn?.[context]?.fr
+        : pn?.[Default]?.fr
+    )
+    || valueName
+  }
+
+  return {
+    locale,
+    config,
+    settings,
+    articles,
+    translations,
+    theme,
+    explorerModeEnabled,
+    favoritesModeEnabled,
+    init,
+    p,
+    pv,
+  }
 })
