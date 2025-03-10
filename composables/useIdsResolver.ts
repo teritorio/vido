ebf0ebe02e1d89d73074450e4b68ddf28f0d41ed
Noé Viricel <noe.viricel@wazo-lab.io>
2025-03-10 13:51:11 +0100

    fix: add address id to regex #509

--- composables/useIdsResolver.ts
+++ composables/useIdsResolver.ts
@@ -1,4 +1,4 @@
-export const regexForCategoryIds = /^(?:(?<cartocode>cartocode:\w{2})|(?<reference>ref:[\w-]+:\w+)|(?<osm>osm:[nwr]\d+)|\d+(?:,\d+)*)$/
+export const regexForCategoryIds = /^(?:(?<cartocode>cartocode:\w{2})|(?<reference>ref:[\w-]+:\w+)|(?<osm>osm:[nwr]\d+)|\d+(?:,\d+)*|(?<address>\d+_\d+))$/
 export const regexForPOIIds = /^(?:cartocode:\w{2}|ref:[\w-]+:\w+|osm:[nwr]\d+|\d+)$/
 
 export function useIdsResolver() {
