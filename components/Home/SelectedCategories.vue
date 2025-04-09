0a04a4c45525869f68c9cf2e4fa63bd0040bddd8
Noé Viricel <noe.viricel@wazo-lab.io>
2025-04-09 16:00:30 +0200

    refactor: apply script setup on SelectedCategories #394

--- components/Home/SelectedCategories.vue
+++ components/Home/SelectedCategories.vue
@@ -1,28 +1,12 @@
-<script lang="ts">
+<script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { mapActions, mapState } from 'pinia'
-
-import { defineNuxtComponent } from '#app'
+import { storeToRefs } from 'pinia'
 import TeritorioIconBadge from '~/components/UI/TeritorioIconBadge.vue'
-import { menuStore } from '~/stores/menu'
+import { menuStore as useMenuStore } from '~/stores/menu'
 
-export default defineNuxtComponent({
-  components: {
-    FontAwesomeIcon,
-    TeritorioIconBadge,
-  },
-
-  computed: {
-    ...mapState(menuStore, ['selectedCategories']),
-  },
-
-  methods: {
-    ...mapActions(menuStore, [
-      'delSelectedCategoryIds',
-      'clearSelectedCategoryIds',
-    ]),
-  },
-})
+const { t } = useI18n()
+const menuStore = useMenuStore()
+const { selectedCategories } = storeToRefs(menuStore)
 </script>
 
 <template>
@@ -39,30 +23,23 @@ export default defineNuxtComponent({
           v-for="menuItem in selectedCategories"
           :id="`selected-category-${menuItem.id}`"
           :key="menuItem.id"
+          :title="(menuItem.menu_group || menuItem.link || menuItem.category).name.fr"
           class="tw-m-1 tw-relative"
-          :title="
-            (menuItem.menu_group || menuItem.link || menuItem.category).name.fr
-          "
         >
           <TeritorioIconBadge
             :id="menuItem.id"
-            :color-fill="
-              (menuItem.menu_group || menuItem.link || menuItem.category)
-                .color_fill
-            "
-            :picto="
-              (menuItem.menu_group || menuItem.link || menuItem.category).icon
-            "
+            :color-fill="(menuItem.menu_group || menuItem.link || menuItem.category).color_fill"
+            :picto="(menuItem.menu_group || menuItem.link || menuItem.category).icon"
             size="lg"
           />
           <button
             type="button"
             class="tw-flex tw-items-center tw-justify-center tw-text-white tw-text-center tw-rounded-full tw-absolute -tw-top-1 -tw-right-1 tw-w-5 tw-h-5 tw-border-solid tw-border-2 tw-border-white tw-bg-red-600 hover:tw-bg-red-800"
-            :title="$t('headerMenu.hideCategory')"
-            @click="delSelectedCategoryIds([menuItem.id])"
+            :title="t('headerMenu.hideCategory')"
+            @click="menuStore.delSelectedCategoryIds([menuItem.id])"
           >
             <span class="tw-sr-only">{{
-              $t('headerMenu.disableCategory')
+              t('headerMenu.disableCategory')
             }}</span>
             <FontAwesomeIcon icon="times" class="tw-text-white" size="sm" />
           </button>
@@ -73,9 +50,9 @@ export default defineNuxtComponent({
       v-if="selectedCategories.length > 1"
       type="button"
       class="tw-absolute -tw-top-3 -tw-right-3 tw-pointer-events-auto tw-flex tw-items-center tw-justify-center tw-text-white tw-text-center tw-rounded-full tw-w-7 tw-h-7 tw-border-solid tw-border-2 tw-border-white tw-bg-red-600 hover:tw-bg-red-800"
-      :title="$t('headerMenu.clearAllCategories')"
-      :aria-label="$t('headerMenu.clearAllCategories')"
-      @click="clearSelectedCategoryIds"
+      :title="t('headerMenu.clearAllCategories')"
+      :aria-label="t('headerMenu.clearAllCategories')"
+      @click="menuStore.clearSelectedCategoryIds"
     >
       <FontAwesomeIcon icon="times" class="tw-text-white" />
     </button>
