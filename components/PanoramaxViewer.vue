4c71ce9bd8e53b878f77d0a49b65fe41c6f33dc1
Noé Viricel <noe.viricel@wazo-lab.io>
2025-04-17 18:56:05 +0200

    feat: add basic panoramax implem

---
+++ components/PanoramaxViewer.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Viewer } from '@panoramax/web-viewer'
+import '@panoramax/web-viewer/build/index.css'
+
+const props = defineProps<{
+  apiEndpoint: string
+  photoId: string
+}>()
+
+const viewerRef = ref<HTMLDivElement>()
+const { locale } = useI18n()
+
+onMounted(() => {
+  viewerRef.value = new Viewer(
+    viewerRef.value,
+    props.apiEndpoint,
+    {
+      map: false,
+      selectedPicture: props.photoId,
+      lang: locale.value,
+    },
+  )
+})
+</script>
+
+<template>
+  <div ref="viewerRef" />
+</template>
---
+++ pages/pano.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import PanoramaxViewer from '~/components/PanoramaxViewer'
+</script>
+
+<template>
+  <div>
+    <h1>Panoramax Demo</h1>
+    <ClientOnly>
+      <PanoramaxViewer
+        api-endpoint="https://api.panoramax.xyz/api"
+        photo-id="a6bde890-797e-4902-b5c7-7ea62f9e45ee"
+      />
+    </ClientOnly>
+  </div>
+</template>
