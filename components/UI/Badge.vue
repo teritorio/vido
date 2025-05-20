d9018511ea56759a4ced4caf075eaa750110da66
Noé Viricel <noe.viricel@wazo-lab.io>
2025-05-20 15:34:08 +0200

    refactor: apply script setup on Badge #563

--- components/UI/Badge.vue
+++ components/UI/Badge.vue
@@ -1,27 +1,28 @@
-<script lang="ts">
-import { defineNuxtComponent } from '#app'
-
-export default defineNuxtComponent({
-  props: {
-    items: {
-      type: Number,
-      default: 0,
-    },
-  },
-
-  computed: {
-    hasItems(): boolean {
-      return this.items > 0
-    },
-  },
+<script setup lang="ts">
+withDefaults(defineProps<{
+  items: number
+}>(), {
+  items: 0,
 })
 </script>
 
 <template>
-  <span
-    v-if="hasItems"
-    class="tw-flex tw-items-center tw-justify-center tw-text-white tw-text-center tw-rounded-full tw-min-w-5 tw-h-5 tw-border-2 tw-border-white tw-bg-red-600 tw-px-1"
-  >
-    <span class="tw-text-xs">{{ items }}</span>
+  <span class="badge">
+    {{ items }}
   </span>
 </template>
+
+<style lang="css" scoped>
+.badge {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #FFF;
+  border-radius: 50%;
+  min-width: 1.25rem;
+  height: 1.25rem;
+  border: 2px solid #FFF;
+  background-color: #DC2626;
+  font-size: 0.75rem;
+}
+</style>
