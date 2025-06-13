108a40e0010ff9642ff82ec5d2437f8ea1fbea7e
Noé Viricel <noe.viricel@wazo-lab.io>
2025-06-14 00:29:53 +0200

    feat: add popup on poi click #503

--- components/Map/MapPois.vue
+++ components/Map/MapPois.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import type { LngLatLike, Map as MapGL } from 'maplibre-gl'
+import { Popup } from 'maplibre-gl'
+import { storeToRefs } from 'pinia'
 import { getBBox } from '~/lib/bbox'
 import MapBase from '~/components/Map/MapBase.vue'
 import type { ApiPoi } from '~/lib/apiPois'
 import { MAP_ZOOM } from '~/lib/constants'
 import type { MapPoiId } from '~/lib/mapPois'
 import { filterRouteByPoiIds } from '~/utils/styles'
+import { mapStore as useMapStore } from '~/stores/map'
 
 const props = withDefaults(defineProps<{
   extraAttributions?: string[]
@@ -27,6 +30,8 @@ const map = ref<MapGL>()
 const selectionZoom = computed(() => MAP_ZOOM.selectionZoom)
 const bounds = computed(() => getBBox({ type: 'FeatureCollection', features: props.features }))
 const center = computed((): LngLatLike | undefined => bounds.value?.getCenter())
+const popup = ref<Popup | null>(null)
+const { teritorioCluster } = storeToRefs(useMapStore())
 
 function onMapInit(mapInstance: MapGL): void {
   map.value = mapInstance
@@ -57,6 +62,18 @@ function renderPois(): void {
     ['get', 'color_fill', ['get', 'display']],
     '#000000',
   ], props.cluster)
+
+  teritorioCluster.value?.addEventListener('feature-click', (e: Event) => {
+    if (popup.value) {
+      popup.value.remove()
+      popup.value = null
+    }
+    const feature = (e as CustomEvent).detail.selectedFeature
+    popup.value = new Popup()
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML(`<h3>${feature.properties.name}</h3>`)
+      .addTo(map.value as MapGL)
+  })
 }
 </script>
 
@@ -74,3 +91,15 @@ function renderPois(): void {
     @map-style-load="onMapStyleLoad"
   />
 </template>
+
+<style lang="css" scoped>
+:deep(.maplibregl-popup-close-button) {
+  width: 24px;
+  height: 24px;
+  font-size: 24px;
+  border: 1px solid #000;
+  border-radius: 50%;
+  margin-top: 4px;
+  margin-right: 4px;
+}
+</style>
