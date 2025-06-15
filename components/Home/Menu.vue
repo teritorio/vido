ded95e4a37df9a38f4b1379692fcce09c99e95f0
Noé Viricel <noe.viricel@wazo-lab.io>
2025-06-15 13:21:25 +0200

    refactor: apply script setup style to Menu #568

--- components/Home/Menu.vue
+++ components/Home/Menu.vue
@@ -1,177 +1,136 @@
-<script lang="ts">
-import type {
-  FontAwesomeIconProps,
-} from '@fortawesome/vue-fontawesome'
-import {
-  FontAwesomeIcon,
-} from '@fortawesome/vue-fontawesome'
-import { mapActions, mapState } from 'pinia'
-import type { PropType } from 'vue'
-
-import { defineNuxtComponent } from '#app'
-import MenuBlock from '~/components/Home/MenuBlock.vue'
-import MenuBlockBottom from '~/components/Home/MenuBlockBottom.vue'
+<script setup lang="ts">
+import type { FontAwesomeIconProps } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { storeToRefs } from 'pinia'
 import FilterCompo from '~/components/Menu/Filter.vue'
 import ItemList from '~/components/Menu/ItemList.vue'
-import Search from '~/components/Search/Search.vue'
-import Logo from '~/components/UI/Logo.vue'
 import type { ApiMenuCategory, MenuGroup, MenuItem } from '~/lib/apiMenu'
-import { menuStore } from '~/stores/menu'
+import { menuStore as useMenuStore } from '~/stores/menu'
+import MenuBlock from '~/components/Home/MenuBlock.vue'
+import MenuBlockBottom from '~/components/Home/MenuBlockBottom.vue'
 
-export default defineNuxtComponent({
-  components: {
-    FontAwesomeIcon,
-    Logo,
-    MenuBlock,
-    MenuBlockBottom,
-    ItemList,
-    FilterCompo,
-    Search,
-  },
-
-  props: {
-    menuBlock: {
-      type: String as PropType<'MenuBlock' | 'MenuBlockBottom'>,
-      required: true,
-    },
-    isOnSearch: {
-      type: Boolean,
-      default: false,
-    },
-    isFilterActive: {
-      type: Boolean,
-      default: false,
-    },
-  },
-
-  data(): {
-    navigationParentIdStack: MenuItem['id'][]
-    categoryIdFilter: ApiMenuCategory['id'] | null
-  } {
-    return {
-      navigationParentIdStack: [],
-      categoryIdFilter: null,
-    }
-  },
-
-  computed: {
-    ...mapState(menuStore, ['filters', 'selectedCategoryIds', 'menuItems']),
-
-    currentParentId(): MenuItem['id'] | undefined {
-      return this.navigationParentIdStack.at(-1)
-    },
-
-    currentMenuItems(): MenuItem[] {
-      return this.getMenuItemByParentId(this.currentParentId)
-    },
-
-    isRootMenu(): boolean {
-      return this.navigationParentIdStack.length === 0
-    },
-
-    size(): FontAwesomeIconProps['size'] {
-      return this.isRootMenu ? '2xl' : 'lg'
-    },
-
-    isAllSelected(): boolean {
-      return this.getRecursiveCategoryIdByParentId(this.currentParentId).every(
-        categoryId => this.selectedCategoryIds.includes(categoryId),
-      )
-    },
-
-    categoriesActivesCountByParent(): Record<ApiMenuCategory['id'], number> {
-      const counts: { [id: string]: number } = {}
-      this.selectedCategoryIds.forEach((categoryId: ApiMenuCategory['id']) => {
-        let parentId = this.menuItems?.[categoryId]?.parent_id
-        while (parentId) {
-          counts[parentId] = (counts[parentId] || 0) + 1
-          parentId = this.menuItems?.[parentId].parent_id
-        }
-      })
-      return counts
-    },
-
-    hasSlot(): boolean {
-      return this.$slots.default !== undefined
-    },
-  },
-
-  emits: {
-    activateFilter: (_val: boolean) => true,
-    scrollTop: () => true,
-  },
-
-  watch: {
-    currentMenuItems() {
-      this.$emit('scrollTop')
-    },
-  },
-
-  methods: {
-    ...mapActions(menuStore, [
-      'addSelectedCategoryIds',
-      'delSelectedCategoryIds',
-      'toggleSelectedCategoryId',
-    ]),
-
-    getMenuItemByParentId(
-      menuGroupId: MenuGroup['id'] | undefined,
-    ): MenuItem[] {
-      return Object.values(this.menuItems || {})
-        .filter(c => (c.parent_id || null) === (menuGroupId || null))
-        .sort((a, b) => a.index_order - b.index_order)
-    },
-
-    getRecursiveCategoryIdByParentId(
-      menuGroupId: MenuGroup['id'] | undefined,
-    ): ApiMenuCategory['id'][] {
-      const menuItems = this.getMenuItemByParentId(menuGroupId)
-      const menuGroups = menuItems.filter(menuItem => menuItem.menu_group)
-      const categories = menuItems
-        .filter(menuItem => menuItem.category)
-        .map(category => category.id)
-      return [
-        ...categories,
-        ...menuGroups
-          .map(parent => this.getRecursiveCategoryIdByParentId(parent.id))
-          .flat(),
-      ].sort()
-    },
-
-    onClickSelectAll(): void {
-      this.addSelectedCategoryIds(
-        this.getRecursiveCategoryIdByParentId(this.currentParentId),
-      )
-    },
-
-    onClickUnselectAll(): void {
-      this.delSelectedCategoryIds(
-        this.getRecursiveCategoryIdByParentId(this.currentParentId),
-      )
-    },
-
-    onGoBackClick() {
-      this.navigationParentIdStack.pop()
-    },
-
-    onMenuGroupClick(menuGroupId: MenuGroup['id']) {
-      this.navigationParentIdStack.push(menuGroupId)
-    },
-
-    onCategoryFilterClick(categoryId: ApiMenuCategory['id']) {
-      this.categoryIdFilter = categoryId
-    },
-
-    onBackToCategoryClick() {
-      this.categoryIdFilter = null
-    },
-  },
+const props = withDefaults(defineProps<{
+  menuBlock: keyof typeof componentMap
+  isOnSearch?: boolean
+  isFilterActive?: boolean
+}>(), {
+  isOnSearch: false,
+  isFilterActive: false,
 })
+
+const emit = defineEmits<{
+  (e: 'activateFilter', val: boolean): void
+  (e: 'scrollTop'): void
+}>()
+
+const componentMap = {
+  MenuBlock,
+  MenuBlockBottom,
+} as const
+
+const slots = useSlots()
+const menuStore = useMenuStore()
+const { filters, selectedCategoryIds, menuItems } = storeToRefs(menuStore)
+const navigationParentIdStack = ref<MenuItem['id'][]>([])
+const categoryIdFilter = ref<ApiMenuCategory['id'] | null>(null)
+
+const dynamicComponent = computed(() => componentMap[props.menuBlock])
+
+const currentParentId = computed((): MenuItem['id'] | undefined => {
+  return navigationParentIdStack.value.at(-1)
+})
+
+const currentMenuItems = computed((): MenuItem[] => {
+  return getMenuItemByParentId(currentParentId.value)
+})
+
+const isRootMenu = computed((): boolean => {
+  return navigationParentIdStack.value.length === 0
+})
+
+const size = computed((): FontAwesomeIconProps['size'] => {
+  return isRootMenu.value ? '2xl' : 'lg'
+})
+
+const isAllSelected = computed((): boolean => {
+  return getRecursiveCategoryIdByParentId(currentParentId.value).every(
+    categoryId => selectedCategoryIds.value.includes(categoryId),
+  )
+})
+
+const categoriesActivesCountByParent = computed((): Record<ApiMenuCategory['id'], number> => {
+  const counts: { [id: string]: number } = {}
+
+  selectedCategoryIds.value.forEach((categoryId: ApiMenuCategory['id']) => {
+    let parentId = menuItems?.value?.[categoryId]?.parent_id
+    while (parentId) {
+      counts[parentId] = (counts[parentId] || 0) + 1
+      parentId = menuItems?.value?.[parentId].parent_id
+    }
+  })
+  return counts
+})
+
+const hasSlot = computed((): boolean => {
+  return slots.default !== undefined
+})
+
+watch(currentMenuItems, () => {
+  emit('scrollTop')
+})
+
+function getMenuItemByParentId(menuGroupId: MenuGroup['id'] | undefined): MenuItem[] {
+  return Object.values(menuItems?.value || {})
+    .filter(c => (c.parent_id || null) === (menuGroupId || null))
+    .sort((a, b) => a.index_order - b.index_order)
+}
+
+function getRecursiveCategoryIdByParentId(menuGroupId: MenuGroup['id'] | undefined): ApiMenuCategory['id'][] {
+  const menuItems = getMenuItemByParentId(menuGroupId)
+  const menuGroups = menuItems.filter(menuItem => menuItem.menu_group)
+  const categories = menuItems
+    .filter(menuItem => menuItem.category)
+    .map(category => category.id)
+  return [
+    ...categories,
+    ...menuGroups
+      .map(parent => getRecursiveCategoryIdByParentId(parent.id))
+      .flat(),
+  ].sort()
+}
+
+function onClickSelectAll(): void {
+  menuStore.addSelectedCategoryIds(
+    getRecursiveCategoryIdByParentId(currentParentId.value),
+  )
+}
+
+function onClickUnselectAll(): void {
+  menuStore.delSelectedCategoryIds(
+    getRecursiveCategoryIdByParentId(currentParentId.value),
+  )
+}
+
+function onGoBackClick() {
+  navigationParentIdStack.value.pop()
+}
+
+function onMenuGroupClick(menuGroupId: MenuGroup['id']) {
+  navigationParentIdStack.value.push(menuGroupId)
+}
+
+function onCategoryFilterClick(categoryId: ApiMenuCategory['id']) {
+  categoryIdFilter.value = categoryId
+}
+
+function onBackToCategoryClick() {
+  categoryIdFilter.value = null
+}
 </script>
 
 <template>
   <component
-    :is="menuBlock"
+    :is="dynamicComponent"
     v-if="categoryIdFilter"
     :is-filter-active="isFilterActive"
   >
@@ -196,11 +155,11 @@ export default defineNuxtComponent({
 
   <div v-else-if="isRootMenu" class="tw-flex tw-flex-col tw-space-y-4">
     <template v-for="(menuItem, index) in currentMenuItems" :key="menuItem.id">
-      <component :is="menuBlock" v-if="index === 0 && hasSlot">
+      <component :is="dynamicComponent" v-if="index === 0 && hasSlot">
         <slot />
       </component>
       <component
-        :is="menuBlock"
+        :is="dynamicComponent"
         v-else-if="index !== 0 && !isOnSearch"
         :class="[index === 0 && 'tw-hidden md:tw-block']"
       >
@@ -213,7 +172,7 @@ export default defineNuxtComponent({
           display-mode-default="compact"
           class="tw-flex-1 tw-pointer-events-auto tw-h-full"
           @menu-group-click="onMenuGroupClick"
-          @category-click="toggleSelectedCategoryId($event)"
+          @category-click="menuStore.toggleSelectedCategoryId($event)"
           @filter-click="onCategoryFilterClick"
         />
       </component>
@@ -221,7 +180,7 @@ export default defineNuxtComponent({
   </div>
 
   <div v-else>
-    <component :is="menuBlock">
+    <component :is="dynamicComponent">
       <div class="tw-w-full tw-flex tw-justify-between tw-pb-4">
         <button
           type="button"
@@ -264,7 +223,7 @@ export default defineNuxtComponent({
         display-mode-default="large"
         class="tw-flex-1 tw-pointer-events-auto tw-h-full"
         @menu-group-click="onMenuGroupClick"
-        @category-click="toggleSelectedCategoryId($event)"
+        @category-click="menuStore.toggleSelectedCategoryId($event)"
         @filter-click="onCategoryFilterClick"
       />
     </component>
