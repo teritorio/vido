f32544f09033a295602eb886bc0e78f4a5ec9e17
Noé Viricel <noe.viricel@wazo-lab.io>
2025-08-11 15:05:48 +0200

    fix: support ref ids #642

--- composables/useIdsResolver.ts
+++ composables/useIdsResolver.ts
@@ -1,5 +1,5 @@
-export const regexForCategoryIds = /^(?:(?<cartocode>cartocode:\w{2})|(?<reference>ref:[\w-]+:\w+)|(?<osm>osm:[nwr]\d+)|\d+(_[a-zA-Z0-9]+)*(?:,\d+)*)$/
-export const regexForPOIIds = /^(?:cartocode:\w{2}|ref:[\w-]+:\w+|osm:[nwr]\d+|\d+(_[a-zA-Z0-9]+)*)$/
+export const regexForCategoryIds = /^(?:(?<cartocode>cartocode:\w{2})|(?<reference>ref:[^:]+(:[^:]+){1,2})|(?<osm>osm:[nwr]\d+)|\d+(_[a-zA-Z0-9]+)*(?:,\d+)*)$/
+export const regexForPOIIds = /^(?:cartocode:\w{2}|ref:[^:]+(:[^:]+){1,2}|osm:[nwr]\d+|\d+(_[a-zA-Z0-9]+)*)$/
 
 export function useIdsResolver() {
 
