058a2802c35624afaa5c77800b58e8b2eac39cf7
Noé Viricel <noe.viricel@wazo-lab.io>
2025-08-14 19:23:16 +0200

    feat: create custom error page #647

---
+++ error.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { VEmptyState } from 'vuetify/lib/labs/components.mjs'
+import { useSiteStore } from './stores/site'
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+
+const { settings } = storeToRefs(useSiteStore())
+
+const logoUrl = computed(() => {
+  return settings.value?.themes[0]?.logo_url || ''
+})
+
+const handleError = () => reloadNuxtApp({ path: '/' })
+</script>
+
+<template>
+  <NuxtLayout name="error">
+    <v-container :style="{ height: '100vh' }">
+      <VEmptyState
+        v-if="error"
+        color="elevated"
+        :image="logoUrl"
+        :headline="$t('error.statusCode', { status: error.statusCode })"
+        :text="error.message"
+        :action-text="$t('error.backHome')"
+        @click:action="handleError"
+      />
+    </v-container>
+  </NuxtLayout>
+</template>
+
+<style lang="css" scoped>
+:deep(.v-empty-state__text) {
+  word-break: break-word;
+  background-color: #202020;
+  color: #d4d4d4;
+  box-shadow: 0 4px 6px rgb(0 0 0 / 10%);
+  border: 1px solid #333;
+  border-radius: 4px;
+  padding: 15px;
+}
+
+:deep(.v-empty-state__media) {
+  width: 180px;
+}
+</style>
