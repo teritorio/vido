9bc2d82c089f1a5687a56efc0be8369de6cbe82c
Noé Viricel <noe.viricel@wazo-lab.io>
2025-10-14 11:53:59 +0200

    refactor: apply script setup style on DateRange #662

--- utils/utilities.ts
+++ utils/utilities.ts
@@ -1,6 +1,47 @@
-import type { ApiPoi, ApiPoiId } from '~/lib/apiPois'
+import type { ApiPoi, ApiPoiId, FieldsListItem } from '~/lib/apiPois'
 import type { ApiAddrSearchResult } from '~/lib/apiSearch'
 import { MAP_ZOOM } from '~/lib/constants'
+import { ADDRESS_FIELDS } from '~/composables/useField'
+
+export function isFiledEmpty(
+  field: FieldsListItem,
+  properties: { [key: string]: string },
+  geom: GeoJSON.Geometry,
+): boolean {
+  if (field.field === 'route') {
+    return !(
+      Object.entries(properties || {})
+        .map(([key, value]) => [key.split(':'), value])
+        .filter(([keys, _value]) => keys[0] === 'route' && keys.length === 3)
+        .length > 0
+    )
+  }
+
+  if (field.field === 'addr') {
+    return ADDRESS_FIELDS.reduce(
+      (sum: boolean, value) => sum || value in properties,
+      false,
+    )
+  }
+
+  if (field.field === 'start_end_date')
+    return isDateRangeEmpty(properties)
+
+  if (field.field === 'coordinates')
+    return isCoordinatesEmpty(geom)
+
+  return !(field.field in properties)
+}
+
+export function isCoordinatesEmpty(geom: GeoJSON.Geometry): boolean {
+  return !(geom && geom.type === 'Point' && geom.coordinates)
+}
+
+export function isDateRangeEmpty(properties: {
+  [key: string]: string
+}): boolean {
+  return !('start' in properties) && !('end' in properties)
+}
 
 export function flattenFeatures(features: { [categoryId: number]: ApiPoi[] }) {
   return Object.values(features)
