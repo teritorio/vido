ccf8edbf8014fff692add28de9300247971f13e8
Noé Viricel <noe.viricel@wazo-lab.io>
2025-10-14 15:11:50 +0200

    refactor: apply script setup style on Select #364

--- components/Filters/Select.vue
+++ components/Filters/Select.vue
@@ -1,83 +1,61 @@
-<script lang="ts">
-import type { PropType } from 'vue'
+<script setup lang="ts">
 import { VAutocomplete } from 'vuetify/components/VAutocomplete'
-
-import { defineNuxtComponent } from '#app'
 import type { FilterValueList } from '~/utils/types-filters'
 
-export default defineNuxtComponent({
-  components: {
-    VAutocomplete,
-  },
+const props = defineProps<{
+  filter: FilterValueList
+}>()
 
-  props: {
-    filter: {
-      type: Object as PropType<FilterValueList>,
-      required: true,
-    },
-  },
+const emit = defineEmits<{
+  (e: 'click'): void
+  (e: 'blur'): void
+  (e: 'change', value?: string[]): void
+}>()
 
-  emits: {
-    click: () => true,
-    blur: () => true,
-    change: (_value: string[] | undefined) => true,
-  },
+const { locale, t } = useI18n()
+const currentValue = ref(props.filter.filterValues)
 
-  data(): {
-    currentValue: string[] | undefined
-  } {
-    return {
-      currentValue: this.filter.filterValues,
-    }
-  },
-
-  computed: {
-    items(): { title: string, value: string }[] {
-      return this.filter.def.values.map(value => ({
-        title: (value.name && value.name.fr) || value.value,
-        value: value.value,
-      })).sort((a, b) => a.title.localeCompare(b.title, this.$i18n.locale, { sensitivity: 'base' }))
-    },
-  },
-
-  watch: {
-    filter() {
-      this.currentValue = this.filter.filterValues
-    },
-  },
-
-  methods: {
-    normalize(str: string): string {
-      return str
-        .toLocaleLowerCase()
-        .normalize('NFD')
-        .replace(/\p{Diacritic}/gu, '')
-    },
-
-    filterEasy(value: string, query: string) {
-      if (value == null || query == null)
-        return -1
-
-      return this.normalize(value).indexOf(this.normalize(query))
-    },
-
-    onChange(value: string[] | undefined) {
-      this.$emit('change', value)
-    },
-  },
+const items = computed((): { title: string, value: string }[] => {
+  return props.filter.def.values.map(value => ({
+    title: (value.name && value.name.fr) || value.value,
+    value: value.value,
+  })).sort((a, b) => a.title.localeCompare(b.title, locale.value, { sensitivity: 'base' }))
 })
+
+watch(() => props.filter, (newValue) => {
+  if (newValue)
+    currentValue.value = newValue.filterValues
+})
+
+function normalize(str: string): string {
+  return str
+    .toLocaleLowerCase()
+    .normalize('NFD')
+    .replace(/\p{Diacritic}/gu, '')
+}
+
+function filterEasy(value: string, query: string) {
+  if (value == null || query == null)
+    return -1
+
+  return normalize(value).indexOf(normalize(query))
+}
+
+function onChange(value: string[] | undefined) {
+  emit('change', value)
+}
 </script>
 
 <template>
   <div>
-    <v-autocomplete
+    <VAutocomplete
       v-model="currentValue"
       outlined
       multiple
       chips
       deletable-chips
       :items="items"
-      :label="$t('listFilter.label')"
+      :label="t('listFilter.label')"
       :clearable="true"
       hide-details="auto"
       density="compact"
