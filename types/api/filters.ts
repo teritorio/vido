8e85fb3532538eee2f281027c516d331ee227478
Noé Viricel <noe.viricel@wazo-lab.io>
2026-01-06 00:01:15 +0100

    chore: move filters types #716

---
+++ types/api/filters.ts
@@ -0,0 +1,32 @@
+export interface FilterList {
+  type: 'multiselection' | 'checkboxes_list'
+  property: string
+  name: MultilingualString
+  values: Array<{
+    value: string
+    name: MultilingualString
+  }>
+}
+
+export interface FilterBoolean {
+  type: 'boolean'
+  property: string
+  name: MultilingualString
+}
+
+export interface FilterDate {
+  type: 'date_range'
+  property_begin: string
+  property_end: string
+  name: MultilingualString
+}
+
+export interface FilterNumberRange {
+  type: 'number_range'
+  property: string
+  name: MultilingualString
+  min: number
+  max: number
+}
+
+export type Filters = FilterList | FilterBoolean | FilterDate | FilterNumberRange
