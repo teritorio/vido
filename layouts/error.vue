d1259733af4c2242a4df83bdb2ba10be1bd035f5
Noé Viricel <noe.viricel@wazo-lab.io>
2026-01-28 15:31:13 +0100

    fix: settings api call undefined on project page

--- layouts/error.vue
+++ layouts/error.vue
@@ -17,30 +17,33 @@ if (configError.value) {
 
 const { settings } = storeToRefs(useSiteStore())
 const apiEndpoint = useState('api-endpoint', () => context.value?.api)
-const { data, error, status } = await useAsyncData('parallel', async () => $fetch<Settings>(`${apiEndpoint.value}/settings.json`))
 
-if (error.value)
-  throw createError(error.value)
+if (apiEndpoint.value) {
+  const { data, error, status } = await useAsyncData('parallel', async () => $fetch<Settings>(`${apiEndpoint.value}/settings.json`))
 
-if (status.value === 'success' && data.value) {
-  settings.value = Object.assign(
-    {
-      id: 0,
-      slug: '',
-      name: '',
-      attribution: [],
-      icon_font_css_url: '',
-      bbox_line: {
-        type: 'LineString',
-        coordinates: [
-          [1.43862, 42.41845],
-          [1.68279, 42.6775],
-        ],
+  if (error.value)
+    throw createError(error.value)
+
+  if (status.value === 'success' && data.value) {
+    settings.value = Object.assign(
+      {
+        id: 0,
+        slug: '',
+        name: '',
+        attribution: [],
+        icon_font_css_url: '',
+        bbox_line: {
+          type: 'LineString',
+          coordinates: [
+            [1.43862, 42.41845],
+            [1.68279, 42.6775],
+          ],
+        },
+        themes: {},
       },
-      themes: {},
-    },
-    data.value,
-  )
+      data.value,
+    )
+  }
 }
 </script>
 
